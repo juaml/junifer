40518369cacab3381f7880d8621258ca33cdaa38
LeSasse <LeSasse@users.noreply.github.com>
2022-01-26 09:24:44 +0000

    deploy: 30d4ce0f92e357aa75501b9bb82f95f8f995f619

--- main/_downloads/a8a9c21c2e94466a6617dd3cbdf8509f/norun_hcpfc_pearson.py
+++ main/_downloads/a8a9c21c2e94466a6617dd3cbdf8509f/norun_hcpfc_pearson.py
@@ -8,6 +8,18 @@ License: BSD 3 clause
 
 from junifer.api import run_pipeline
 
+custom_confound_strategy = {
+    'filter': 'butterworth',
+    'detrend': True,
+    'high_pass': 0.01,
+    'low_pass': 0.08,
+    'standardize': True,
+    'confounds': ['csf', 'wm', 'gsr'],
+    'derivatives': True,
+    'squares': True,
+    'other': []
+}
+
 markers = [
     {'name': 'Power264_FCPearson',
      'kind': 'FunctionalConnectivity',
@@ -29,11 +41,25 @@ markers = [
      'atlas': 'Schaefer400x17',
      'method': 'Spearman',
      'confound_strategy': 'path/to/predefined/confound_file.tsv'}
+    {'name': 'Schaefer400x17_FCSpearman',
+     'kind': 'FunctionalConnectivity',
+     'atlas': 'Schaefer400x17',
+     'method': 'Spearman',
+     'confound_strategy': custom_confound_strategy}
 ]
+
+dg_params = {
+    'modality': 'fMRI',
+    'tasks':  ['REST1', 'REST2', 'SOCIAL'],
+    'phase_encoding': ('LR', 'RL'),
+    'preprocessed': 'ICA+FIX',
+    'space': 'volumetric',
+}
 
 run_pipeline(
     workdir='/tmp',
     datagrabber='HCPOpenAccess',
+    datagrabber_params = dg_params,
     element='100408',
     markers=markers,
     storage='SQLDataFrameStorage',
