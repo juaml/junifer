b0edc9198732abbdd486ec876969298d808982e4
Synchon Mandal <synchon@protonmail.com>
2022-07-26 16:30:22 +0200

    fix: flake8 fix for testing.datagrabbers

--- junifer/testing/datagrabbers.py
+++ junifer/testing/datagrabbers.py
@@ -1,5 +1,9 @@
+"""Provide testing datagrabbers."""
+
 # Authors: Federico Raimondo <f.raimondo@fz-juelich.de>
+#          Synchon Mandal <s.mandal@fz-juelich.de>
 # License: AGPL
+
 import tempfile
 from nilearn import datasets
 
@@ -7,18 +11,20 @@ from ..datagrabber.base import BaseDataGrabber
 
 
 class OasisVBMTestingDatagrabber(BaseDataGrabber):
-    """
-    DataGrabber for Oasis VBM testing data.
-    """
+    """DataGrabber for Oasis VBM testing data."""
+
     def __init__(self):
+        """Initialize class."""
         datadir = tempfile.mkdtemp()
         types = ['VBM_GM']
         super().__init__(types=types, datadir=datadir)
 
     def get_elements(self):
+        """Get elements."""
         return [f'sub-{x:02d}' for x in list(range(1, 11))]
 
     def __getitem__(self, element):
+        """Get item implementation."""
         out = super().__getitem__(element)
         i_sub = int(element.split('-')[1]) - 1
         out['VBM_GM'] = {'path': self._dataset.gray_matter_maps[i_sub]}
@@ -27,5 +33,6 @@ class OasisVBMTestingDatagrabber(BaseDataGrabber):
         return out
 
     def __enter__(self):
+        """Context enter implementation."""
         self._dataset = datasets.fetch_oasis_vbm(n_subjects=10)
         return self
