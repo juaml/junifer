99705b8a9b9b733b2e03a28055587a0aafe8e03e
Synchon Mandal <synchon@protonmail.com>
2022-07-27 12:03:19 +0200

    fix: flake fix for utils.fs

--- junifer/utils/fs.py
+++ junifer/utils/fs.py
@@ -1,7 +1,10 @@
+"""Provide functions for filesystem."""
+
 import os
 import stat
 
 
 def make_executable(path):
+    """Make executable."""
     st = os.stat(path)
     os.chmod(path, st.st_mode | stat.S_IEXEC)
