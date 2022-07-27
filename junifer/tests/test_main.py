bfc89ea5d0db8d0dbcbcc76270b6dbb5b836acc7
Synchon Mandal <synchon@protonmail.com>
2022-07-27 12:06:12 +0200

    fix: flake8 fix for tests.test_main

--- junifer/tests/test_main.py
+++ junifer/tests/test_main.py
@@ -1,6 +1,11 @@
+"""Provide tests for package."""
+
 # Authors: Federico Raimondo <f.raimondo@fz-juelich.de>
 #          Leonard Sasse <l.sasse@fz-juelich.de>
 # License: AGPL
+
+
 def test_import():
+    """Test junifer import."""
     import junifer
     print(junifer.__version__)
