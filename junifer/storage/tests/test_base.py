9b24684541fd35c36f9d98c7ffb76150226efcc4
Synchon Mandal <synchon@protonmail.com>
2022-08-10 22:30:11 +0200

    refactor: prune test_base.py and add type annotations

--- junifer/storage/tests/test_base.py
+++ junifer/storage/tests/test_base.py
@@ -1,11 +1,23 @@
 """Provide tests for base."""
 
+# Authors: Federico Raimondo <f.raimondo@fz-juelich.de>
+#          Synchon Mandal <s.mandal@fz-juelich.de>
+# License: AGPL
+
 import pytest
 
 from junifer.storage.base import BaseFeatureStorage
-    with pytest.raises(TypeError, match=r"abstract"):
-        BaseFeatureStorage(uri='/tmp')  # type: ignore
 
+
+def test_BaseFeatureStorage_abstractness() -> None:
+    """Test BaseFeatureStorage is abstract base class."""
+    with pytest.raises(TypeError, match=r"abstract"):
+        BaseFeatureStorage(uri="/tmp")
+
+
+def test_BaseFeatureStorage() -> None:
+    """Test BaseFeatureStorage."""
+    # Create concrete class
     class MyFeatureStorage(BaseFeatureStorage):
         def __init__(self, uri, single_output=False):
             super().__init__(uri, single_output=single_output)
@@ -17,8 +29,7 @@ from junifer.storage.base import BaseFeatureStorage
             super().list_features()
 
         def read_df(self, feature_name=None, feature_md5=None):
-            super().read_df(
-                feature_name=feature_name, feature_md5=feature_md5)
+            super().read_df(feature_name=feature_name, feature_md5=feature_md5)
 
         def store_metadata(self, metadata):
             super().store_metadata(metadata)
@@ -38,10 +49,10 @@ from junifer.storage.base import BaseFeatureStorage
         def collect(self):
             return super().collect()
 
-    st = MyFeatureStorage(uri='/tmp')
+    st = MyFeatureStorage(uri="/tmp")
     assert st.single_output is False
 
-    st = MyFeatureStorage(uri='/tmp', single_output=True)
+    st = MyFeatureStorage(uri="/tmp", single_output=True)
     assert st.single_output is True
 
     with pytest.raises(NotImplementedError):
