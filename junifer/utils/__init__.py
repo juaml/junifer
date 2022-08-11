d5d81aa3056ca6e94fc031cc21fb41d4b88f06f4
Synchon Mandal <synchon@protonmail.com>
2022-08-11 18:48:08 +0200

    refactor: prune import for utils sub-package

--- junifer/utils/__init__.py
+++ junifer/utils/__init__.py
@@ -1,2 +1,8 @@
-from . import logging
-from .logging import configure_logging, logger, raise_error, warn
+"""Provide imports for utils sub-package."""
+
+# Authors: Federico Raimondo <f.raimondo@fz-juelich.de>
+#          Synchon Mandal <s.mandal@fz-juelich.de>
+# License: AGPL
+
+from .fs import make_executable
+from .logging import configure_logging, logger, raise_error, warn_with_log
