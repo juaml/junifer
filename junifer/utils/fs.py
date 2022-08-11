5d9c39d3dc8af8e8c2ef812e1aa51d73e9ca10e9
Synchon Mandal <synchon@protonmail.com>
2022-08-11 15:26:26 +0200

    refactor: add docstrings and type annotations to utils/fs.py

--- junifer/utils/fs.py
+++ junifer/utils/fs.py
@@ -1,10 +1,21 @@
-"""Provide functions for filesystem."""
+"""Provide functions for filesystem manipulation."""
 
-import os
+# Authors: Federico Raimondo <f.raimondo@fz-juelich.de>
+#          Synchon Mandal <s.mandal@fz-juelich.de>
+# License: AGPL
+
 import stat
+from pathlib import Path
 
 
-def make_executable(path):
-    """Make executable."""
-    st = os.stat(path)
-    os.chmod(path, st.st_mode | stat.S_IEXEC)
+def make_executable(path: Path) -> None:
+    """Make `path` executable.
+
+    Parameters
+    ----------
+    path : pathlib.Path
+        The path to make executable.
+
+    """
+    st = path.stat()
+    path.chmod(mode=st.st_mode | stat.S_IEXEC)
