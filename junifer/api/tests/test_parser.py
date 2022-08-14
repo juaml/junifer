3392911493ec0bc719b20de2af7b57e0ba5b561c
Synchon Mandal <synchon@protonmail.com>
2022-08-14 14:24:50 +0200

    refactor: prune, add docstrings and type annotations in test_parser.py

--- junifer/api/tests/test_parser.py
+++ junifer/api/tests/test_parser.py
@@ -1,44 +1,77 @@
 """Provide tests for parser."""
 
+# Authors: Federico Raimondo <f.raimondo@fz-juelich.de>
+#          Synchon Mandal <s.mandal@fz-juelich.de>
+# License: AGPL
+
 import sys
 from pathlib import Path
-import tempfile
+
 import pytest
 
 from junifer.api.parser import parse_yaml
 
 
-def test_parse_yaml():
-    """Test parse yaml."""
-    with pytest.raises(ValueError, match='does not exist'):
-        parse_yaml('foo.yaml')
+def test_parse_yaml_failure() -> None:
+    """Test YAML parsing failure."""
+    with pytest.raises(ValueError, match="does not exist"):
+        parse_yaml("foo.yaml")
 
-    with tempfile.TemporaryDirectory() as _tmpdir:
-        fname = Path(_tmpdir) / 'test.yaml'
 
-        with open(fname, 'w') as f:
-            f.write('foo: bar\n')
-        contents = parse_yaml(fname)
-        assert 'foo' in contents
-        assert 'bar' == contents['foo']
+def test_parse_yaml_success(tmp_path: Path) -> None:
+    """Test YAML parsing success.
 
-        with open(fname, 'w') as f:
-            f.write('foo: bar\n')
-            f.write('with: numpy\n')
+    Parameters
+    ----------
+    tmp_path : pathlib.Path
+        The path to the test directory.
 
-        contents = parse_yaml(fname)
-        assert 'foo' in contents
-        assert 'bar' == contents['foo']
-        assert 'with' in contents
-        assert 'numpy' in contents['with']
+    """
+    # Write test file
+    fname = tmp_path / "test_parse_yaml_success.yaml"
+    fname.write_text("foo: bar")
+    # Check test file
+    contents = parse_yaml(fname)
+    assert "foo" in contents
+    assert contents["foo"] == "bar"
 
-        assert 'junifer.configs.wrong_config' not in sys.modules
 
-        with open(fname, 'w') as f:
-            f.write('foo: bar\n')
-            f.write('with:\n')
-            f.write('  - numpy\n')
-            f.write('  - junifer.testing.wrong_config\n')
+def test_parse_yaml_success_with_module_autoload(tmp_path: Path) -> None:
+    """Test YAML parsing with single module autoload success.
 
-        with pytest.raises(ImportError, match='wrong_config'):
-            contents = parse_yaml(fname.as_posix())
+    Parameters
+    ----------
+    tmp_path : pathlib.Path
+        The path to the test directory.
+
+    """
+    # Write test file
+    fname = tmp_path / "test_parse_yaml_with_single_module_autoload.yaml"
+    fname.write_text("foo: bar\nwith: numpy")
+    # Check test file
+    contents = parse_yaml(fname)
+    assert "foo" in contents
+    assert contents["foo"] == "bar"
+    assert "with" in contents
+    assert contents["with"] == "numpy"
+    assert "numpy" in sys.modules
+    assert "junifer.configs.wrong_config" not in sys.modules
+
+
+def test_parse_yaml_failure_with_multi_module_autoload(tmp_path: Path) -> None:
+    """Test YAML parsing with multi module autoload failure.
+
+    Parameters
+    ----------
+    tmp_path : pathlib.Path
+        The path to the test directory.
+
+    """
+    # Write test file
+    fname = tmp_path / "test_parse_yaml_with_multi_module_autoload.yaml"
+    fname.write_text(
+        "foo: bar\nwith:\n  - numpy\n  - junifer.testing.wrong_config"
+    )
+    # Check test file
+    with pytest.raises(ImportError, match="wrong_config"):
+        parse_yaml(fname)
