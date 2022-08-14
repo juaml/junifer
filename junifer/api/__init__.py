9bc38c58a895604da53a94acec7b863d7458c130
Synchon Mandal <synchon@protonmail.com>
2022-08-14 15:02:01 +0200

    refactor: improve imports for api sub-package

--- junifer/api/__init__.py
+++ junifer/api/__init__.py
@@ -1,2 +1,8 @@
+"""Provide imports for api sub-package."""
+
+# Authors: Federico Raimondo <f.raimondo@fz-juelich.de>
+#          Synchon Mandal <s.mandal@fz-juelich.de>
+# License: AGPL
+
+from .cli import cli
 from .functions import run, collect
-from .import cli
