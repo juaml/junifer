cebf7613ed17443f162181b5ae237d5394cbcc43
Synchon Mandal <synchon@protonmail.com>
2022-08-15 16:17:19 +0200

    refactor: add docstring for datareader package import module

--- junifer/datareader/__init__.py
+++ junifer/datareader/__init__.py
@@ -1,5 +1,8 @@
+"""Provide imports for datareader sub-package."""
+
 # Authors: Federico Raimondo <f.raimondo@fz-juelich.de>
 #          Leonard Sasse <l.sasse@fz-juelich.de>
+#          Synchon Mandal <s.mandal@fz-juelich.de>
 # License: AGPL
 
 from .default import DefaultDataReader
