e0a138a66d3407d1efa4543787526089aeb80006
Synchon Mandal <synchon@protonmail.com>
2022-08-15 16:16:05 +0200

    refactor: add docstrings and type annotations for datareader/default.py

--- junifer/datareader/default.py
+++ junifer/datareader/default.py
@@ -1,47 +1,75 @@
 """Provide class for default data reader."""
 
 # Authors: Federico Raimondo <f.raimondo@fz-juelich.de>
+#          Synchon Mandal <s.mandal@fz-juelich.de>
 # License: AGPL
 
 from pathlib import Path
+from typing import Dict
 
 import nibabel as nib
 import pandas as pd
 
-from ..utils.logging import logger, warn
 from ..markers.base import PipelineStepMixin
+from ..utils.logging import logger, warn_with_log
 
-# Map each filenanm end to a kind
+
+# Map each file extension to a kind
 _extensions = {
-    '.nii': 'NIFTI',
-    '.nii.gz': 'NIFTI',
-    '.csv': 'CSV',
-    '.tsv': 'TSV'
-
+    ".nii": "NIFTI",
+    ".nii.gz": "NIFTI",
+    ".csv": "CSV",
+    ".tsv": "TSV",
 }
 
 # Map each kind to a function and arguments
 _readers = {}
-_readers['NIFTI'] = dict(func=nib.load, params=None)
-_readers['CSV'] = dict(func=pd.read_csv, params=None)
-_readers['TSV'] = dict(func=pd.read_csv, params={'sep': '\t'})
+_readers["NIFTI"] = {"func": nib.load, "params": None}
+_readers["CSV"] = {"func": pd.read_csv, "params": None}
+_readers["TSV"] = {"func": pd.read_csv, "params": {"sep": "\t"}}
 
 
 class DefaultDataReader(PipelineStepMixin):
     """Mixin class for default data reader."""
 
+    # TODO: complete type annotations
     def validate_input(self, input):
-        """Validate input."""
+        """Validate input.
+
+        Parameters
+        ----------
+        input
+
+        """
         # Nothing to validate, any input is fine
         pass
 
+    # TODO: complete type annotations
     def get_output_kind(self, input):
-        """Get output kind."""
+        """Get output kind.
+
+        Parameters
+        ----------
+        input
+
+        """
         # It will output the same kind of data as the input
         return input
 
-    def fit_transform(self, input, params=None):
-        """Fit and transform."""
+    # TODO: complete type annotations
+    def fit_transform(self, input, params=None) -> Dict:
+        """Fit and transform.
+
+        Parameters
+        ----------
+        input
+        params
+
+        Returns
+        -------
+        dict
+
+        """
         # For each kind of data, try to read it
 
         # out is the same, but with the 'data' key set in
@@ -50,39 +78,41 @@ class DefaultDataReader(PipelineStepMixin):
         if params is None:
             params = {}
         for kind in input.keys():
-            if kind == 'meta':
-                out['meta'] = input['meta']
+            if kind == "meta":
+                out["meta"] = input["meta"]
                 continue
-            if 'path' not in input[kind]:
-                warn(
-                    f'Input kind {kind} does not provide a path. Skipping.')
+            if "path" not in input[kind]:
+                warn_with_log(
+                    f"Input kind {kind} does not provide a path. Skipping."
+                )
                 continue
-            t_path = input[kind]['path']
+            t_path = input[kind]["path"]
             t_params = params.get(kind, {})
 
             # Convert to Path if datareader is not well done
             if not isinstance(t_path, Path):
                 t_path = Path(t_path)
-                out[kind]['path'] = t_path
-            logger.info(f'Reading {kind} from {t_path.as_posix()}')
+                out[kind]["path"] = t_path
+            logger.info(f"Reading {kind} from {t_path.as_posix()}")
             fread = None
 
             fname = t_path.name.lower()
             for ext, ftype in _extensions.items():
                 if fname.endswith(ext):
-                    logger.info(f'{kind} is type {ftype}')
-                    reader_func = _readers[ftype]['func']
-                    reader_params = _readers[ftype]['params']
+                    logger.info(f"{kind} is type {ftype}")
+                    reader_func = _readers[ftype]["func"]
+                    reader_params = _readers[ftype]["params"]
                     if reader_params is not None:
                         t_params.update(reader_params)
-                    logger.debug(f'Calling {reader_func} with {t_params}')
+                    logger.debug(f"Calling {reader_func} with {t_params}")
                     fread = reader_func(t_path, **t_params)
                     break
             if fread is None:
                 logger.info(
-                    f'Unknown file type {t_path.as_posix()}, skipping reading')
-            out[kind]['data'] = fread
-            if 'meta' not in out:
-                out['meta'] = {}
-            out['meta']['datareader'] = self.get_meta()
+                    f"Unknown file type {t_path.as_posix()}, skipping reading"
+                )
+            out[kind]["data"] = fread
+            if "meta" not in out:
+                out["meta"] = {}
+            out["meta"]["datareader"] = self.get_meta()
         return out
