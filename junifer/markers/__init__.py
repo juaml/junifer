78105e625ddacb8e15166acf5a10bb452978a63b
Synchon Mandal <synchon@protonmail.com>
2022-08-15 15:42:04 +0200

    refactor: improve imports for markers sub-package

--- junifer/markers/__init__.py
+++ junifer/markers/__init__.py
@@ -1,5 +1,10 @@
+"""Provide imports for markers sub-package."""
+
 # Authors: Federico Raimondo <f.raimondo@fz-juelich.de>
 #          Leonard Sasse <l.sasse@fz-juelich.de>
 # License: AGPL
+
+from .base import BaseMarker
 from .collection import MarkerCollection
 from .parcel import ParcelAggregation
+from .pipeline_mixin import PipelineStepMixin
