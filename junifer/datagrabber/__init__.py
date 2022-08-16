f53e48e74bd4155cbc0cced8a41eceb079d8315f
Synchon Mandal <synchon@protonmail.com>
2022-08-16 17:08:10 +0200

    refactor: improve imports for datagrabber sub-package

--- junifer/datagrabber/__init__.py
+++ junifer/datagrabber/__init__.py
@@ -1,5 +1,12 @@
+"""Provide imports for datagrabber sub-package."""
+
 # Authors: Federico Raimondo <f.raimondo@fz-juelich.de>
 #          Leonard Sasse <l.sasse@fz-juelich.de>
 # License: AGPL
-from .base import (DataladDataGrabber, PatternDataGrabber,
-                   PatternDataladDataGrabber)
+
+from .base import BaseDataGrabber
+from .datalad_base import DataladDataGrabber
+from .hcp import DataladHCP1200, HCP1200
+from .meta import MultipleDataGrabber
+from .pattern import PatternDataGrabber
+from .pattern_datalad_base import PatternDataladDataGrabber
