25313c84131b78f98f07a51aefd71bf9501c6b82
Synchon Mandal <synchon@protonmail.com>
2022-08-16 16:41:03 +0200

    refactor: improve imports for preprocess sub-package

--- junifer/preprocess/__init__.py
+++ junifer/preprocess/__init__.py
@@ -1,3 +1,7 @@
+"""Provide imports for preprocess sub-package."""
+
 # Authors: Federico Raimondo <f.raimondo@fz-juelich.de>
 #          Leonard Sasse <l.sasse@fz-juelich.de>
 # License: AGPL
+
+from .confounds import BaseConfoundRemover
