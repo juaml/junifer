d27f64e9141b810d4264b7a9cfc9313749452dcb
Synchon Mandal <synchon@protonmail.com>
2022-08-16 17:06:48 +0200

    refactor: move DataladDataGrabber to separate module

---
+++ junifer/datagrabber/tests/test_datalad_base.py
@@ -0,0 +1,21 @@
+"""Provide tests for datalad_base."""
+
+# Authors: Synchon Mandal <s.mandal@fz-juelich.de>
+# License: AGPL
+
+import pytest
+
+from junifer.datagrabber.datalad_base import DataladDataGrabber
+
+
+def test_datalad_base_abstractness() -> None:
+    """Test datalad base is abstract."""
+    with pytest.raises(TypeError, match=r"abstract"):
+        DataladDataGrabber()
+
+# def test_datalad_base_missing_uri() -> None:
+#     """Test proper check of missing URI in datalad base initialization."""
+#     with pytest.raises(ValueError, match=r"`uri` must be provided"):
+#         DataladDataGrabber(
+
+#         )
