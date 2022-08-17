e8a8da8ecf99123615b9af4f133f2d43f47f5c8c
Synchon Mandal <synchon@protonmail.com>
2022-08-17 13:56:15 +0200

    refactor: improve imports for configs sub-package

---
+++ junifer/configs/__init__.py
@@ -0,0 +1,8 @@
+"""Provide imports for configs sub-package."""
+
+# Authors: Federico Raimondo <f.raimondo@fz-juelich.de>
+#          Leonard Sasse <l.sasse@fz-juelich.de>
+#          Synchon Mandal <s.mandal@fz-juelich.de>
+# License: AGPL
+
+from .juseless import JuselessDataladUKBVBM
