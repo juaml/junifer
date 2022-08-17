7a2a9b07960b384aa8fc5394ac2d3cd4379459b5
Synchon Mandal <synchon@protonmail.com>
2022-08-17 09:15:51 +0200

    refactor: add docstrings and type annotations in testing/datagrabbers.py

--- junifer/testing/datagrabbers.py
+++ junifer/testing/datagrabbers.py
@@ -5,6 +5,8 @@
 # License: AGPL
 
 import tempfile
+from typing import Dict, List
+
 from nilearn import datasets
 
 from ..datagrabber.base import BaseDataGrabber
@@ -13,26 +15,53 @@ from ..datagrabber.base import BaseDataGrabber
 class OasisVBMTestingDatagrabber(BaseDataGrabber):
     """DataGrabber for Oasis VBM testing data."""
 
-    def __init__(self):
-        """Initialize class."""
+    def __init__(self) -> None:
+        """Initialize the class."""
+        # Create temporary directory
         datadir = tempfile.mkdtemp()
-        types = ['VBM_GM']
+        # Define types
+        types = ["VBM_GM"]
         super().__init__(types=types, datadir=datadir)
 
-    def get_elements(self):
-        """Get elements."""
-        return [f'sub-{x:02d}' for x in list(range(1, 11))]
+    def __getitem__(self, element: str) -> Dict:
+        """Implement indexing support.
 
-    def __getitem__(self, element):
-        """Get item implementation."""
+        Paramters
+        ---------
+        element : str
+            The element to retrieve.
+
+        Returns
+        -------
+        dict
+            The data along with the metadata.
+
+        """
         out = super().__getitem__(element)
-        i_sub = int(element.split('-')[1]) - 1
-        out['VBM_GM'] = {'path': self._dataset.gray_matter_maps[i_sub]}
+        i_sub = int(element.split("-")[1]) - 1
+        out["VBM_GM"] = {"path": self._dataset.gray_matter_maps[i_sub]}
         # Set the element accordingly
-        out['meta']['element'] = {'subject': element}
+        out["meta"]["element"] = {"subject": element}
         return out
 
-    def __enter__(self):
-        """Context enter implementation."""
+    def __enter__(self) -> "OasisVBMTestingDatagrabber":
+        """Implement context entry.
+
+        Returns
+        -------
+        OasisVBMTestingDatagrabber
+
+        """
         self._dataset = datasets.fetch_oasis_vbm(n_subjects=10)
         return self
+
+    def get_elements(self) -> List[str]:
+        """Get elements.
+
+        Returns
+        -------
+        list of str
+            List of elements that can be grabbed.
+
+        """
+        return [f"sub-{x:02d}" for x in list(range(1, 11))]
