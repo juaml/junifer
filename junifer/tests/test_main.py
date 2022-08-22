46bb47e28ce34d01a92146c1911d3295fde74712
Synchon Mandal <synchon@protonmail.com>
2022-08-22 15:01:20 +0200

    refactor: add docstrings and type annotations in test_main.py

--- junifer/tests/test_main.py
+++ junifer/tests/test_main.py
@@ -1,11 +1,12 @@
-"""Provide tests for package."""
+"""Provide tests for junifer package."""
 
 # Authors: Federico Raimondo <f.raimondo@fz-juelich.de>
 #          Leonard Sasse <l.sasse@fz-juelich.de>
+#          Synchon Mandal <s.mandal@fz-juelich.de>
 # License: AGPL
 
 
-def test_import():
+def test_import() -> None:
     """Test junifer import."""
     import junifer
     print(junifer.__version__)
