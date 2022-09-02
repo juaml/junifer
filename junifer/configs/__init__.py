0edafa26af8cea6c67b9977d636314b6389193db
Synchon Mandal <synchon@protonmail.com>
2022-09-02 11:50:11 +0200

    refactor: remove imports from configs/__init__.py

--- junifer/configs/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Provide imports for configs sub-package."""
-
-# Authors: Federico Raimondo <f.raimondo@fz-juelich.de>
-#          Leonard Sasse <l.sasse@fz-juelich.de>
-#          Synchon Mandal <s.mandal@fz-juelich.de>
-# License: AGPL
-
-from .juseless import JuselessDataladUKBVBM
