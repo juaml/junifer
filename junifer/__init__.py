4dd848dab103159451f7af25588f598f5acce8a9
Synchon Mandal <synchon@protonmail.com>
2022-09-06 10:52:46 +0200

    chore: add module docstring for junifer package import

--- junifer/__init__.py
+++ junifer/__init__.py
@@ -1,3 +1,9 @@
+"""Provide imports for junifer package."""
+
+# Authors: Federico Raimondo <f.raimondo@fz-juelich.de>
+#          Synchon Mandal <s.mandal@fz-juelich.de>
+# License: AGPL
+
 from ._version import __version__
 from . import api
 from . import utils
