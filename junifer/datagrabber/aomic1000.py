d988ccf084674884a54dd58718cf7c2156cee0c2
Vera Komeyer <v.komeyer@fz-juelich.de>
2022-09-14 15:43:25 +0200

    format black isort

--- junifer/datagrabber/aomic1000.py
+++ junifer/datagrabber/aomic1000.py
@@ -7,8 +7,6 @@ from typing import Union
 from junifer.datagrabber import PatternDataladDataGrabber
 
 from ..api.decorators import register_datagrabber
-# from ..utils import raise_error
-# from .pattern import PatternDataGrabber
 
 
 @register_datagrabber
@@ -35,26 +33,6 @@ class DataladAOMIC1000(PatternDataladDataGrabber):
         # tasks: Union[str, List[str], None] = None,
         **kwargs,
     ) -> None:
-        # """Initialize the class."""
-        # # All tasks
-        # all_tasks = [
-        #     "moviewatching",
-        # ]
-        # # Set default tasks
-        # if tasks is None:
-        #     self.tasks: List[str] = all_tasks
-        # # Convert single task into list
-        # else:
-        #     if not isinstance(tasks, List):
-        #         tasks = [tasks]
-        #     # Check for invalid task(s)
-        #     for task in tasks:
-        #         if task not in all_tasks:
-        #             raise_error(
-        #                 f"'{task}' is not a valid AOMIC fMRI task input. "
-        #                 f"Valid task values can be {all_tasks}."
-        #             )
-        #     self.tasks: List[str] = tasks
 
         # The types of data
         types = [
@@ -113,64 +91,3 @@ class DataladAOMIC1000(PatternDataladDataGrabber):
             patterns=patterns,
             replacements=replacements,
         )
-
-    # def __getitem__(self, element: Tuple[str, str, str]) -> Dict[str, Path]:
-    #     """Index one element in the dataset.
-
-    #     Parameters
-    #     ----------
-    #     element : triple of str
-    #         The element to be indexed. First element in the tuple is the
-    #         subject, second element is the task.
-
-    #     Returns
-    #     -------
-    #     out : dict
-    #         Dictionary of paths for each type of data required for the
-    #         specified element.
-
-    #     """
-    #     sub, task = element
-
-    #     out = super().__getitem__((sub, task))
-    #     out["meta"]["element"] = {
-    #         "subject": sub,
-    #         # "task": task,
-    #     }
-    #     return out
-
-# @register_datagrabber
-# class DataladAOMIC1000(DataladDataGrabber, AOMIC1000):
-#     """Concrete implementation for datalad-based data fetching of AOMIC1000.
-
-#     Parameters
-#     ----------
-#     datadir : str or Path, optional
-#         The directory where the datalad dataset will be cloned. If None,
-#         the datalad dataset will be cloned into a temporary directory
-#         (default None).
-#     tasks : {"moviewatching"} AOMIC 1000 tasks.
-#         If None, moviewatching is selected.
-#         (default None).
-#     **kwargs
-#         Keyword arguments passed to superclass.
-
-#     """
-
-#     def __init__(
-#         self,
-#         datadir: Union[str, Path, None] = None,
-#         tasks: Union[str, List[str], None] = None,
-#         **kwargs,
-#     ) -> None:
-#         """Initialize the class."""
-#         uri = (
-#             "https://github.com/OpenNeuroDatasets/ds003097.git"
-#         )
-#         rootdir = "derivatives"
-#         super().__init__(
-#             datadir=datadir,
-#             tasks=tasks,
-#             uri=uri,
-#             rootdir=rootdir,
-#         )
