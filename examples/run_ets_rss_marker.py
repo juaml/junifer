96ffdc7496d14ad96d5f24a1a4fa710b444ef4b4
Synchon Mandal <synchon@protonmail.com>
2022-09-14 15:31:47 +0200

    chore: improve example for RSSETSMarker

--- examples/run_ets_rss_marker.py
+++ examples/run_ets_rss_marker.py
@@ -1,24 +1,22 @@
 """
-Run junifer and julearn.
-========================
-This example uses a ParcelAggregation marker to compute the mean of each parcel
-using the Schaefer atlas (100 rois, 7 Yeo networks) for a 3D nifti to extract
-some features for machine learning using julearn to predict some other data.
+
+Extracting root sum of squares from edge-wise timeseries.
+=========================================================
+This example uses a RSSETSMarker to compute root sum of squares
+of the edge-wise timeseries using the Schaefer atlas
+(100 rois and 200 rois, 17 Yeo networks) for a 4D nifti BOLD file.
+
 Authors: Leonard Sasse, Sami Hamdan, Nicolas Nieto, Synchon Mandal
+
 License: BSD 3 clause
+
 """
 
 import tempfile
 
-import nilearn
-import pandas as pd
-from julearn import run_cross_validation
-
-import junifer.testing.registry
-from junifer.api import collect, run
-from junifer.storage.sqlite import SQLiteFeatureStorage
+import junifer.testing.registry  # noqa: F401
 from junifer.utils import configure_logging
-from ptpython.ipython import embed
+
 
 ###############################################################################
 # Set the logging level to info to see extra information:
@@ -44,17 +42,19 @@ marker_dicts = [
 
 ###############################################################################
 # Create a temporary directory for junifer feature extraction:
-# At the end you can read the extracted data into a pandas.DataFrame.
+# At the end you can read the extracted data into a ``pandas.DataFrame``.
 with tempfile.TemporaryDirectory() as tmpdir:
 
     storage = {"kind": "SQLiteFeatureStorage", "uri": f"{tmpdir}/test.db"}
-    # run the defined junifer feature extraction pipeline 
-    run(
-        workdir="/tmp",
-        datagrabber={"kind": "SPMAuditoryTestingDatagrabber"},
-        markers=marker_dicts,
-        storage=storage,
-    )
+    # run the defined junifer feature extraction pipeline
+    # TODO: needs SQLiteFeatureStorage.store_timeseries() to be
+    # implemented first
+    # run(
+    #     workdir="/tmp",
+    #     datagrabber={"kind": "SPMAuditoryTestingDatagrabber"},
+    #     markers=marker_dicts,
+    #     storage=storage,
+    # )
 
     # read in extracted features and add confounds and targets
     # for julearn run cross validation
