43265ba766371bf4fcaeb85b86d7ebc8d39344ef
Vera Komeyer <v.komeyer@fz-juelich.de>
2022-09-14 10:36:22 +0200

    Add BOLD_confounds ANAT DWI types

--- junifer/datagrabber/aomic1000.py
+++ junifer/datagrabber/aomic1000.py
@@ -1,18 +1,18 @@
 """Provide concrete implementations for HCP data access."""
 
-from itertools import product
+# from itertools import product
 from pathlib import Path
 from typing import Dict, List, Tuple, Union
 
-from junifer.datagrabber.datalad_base import DataladDataGrabber
+from junifer.datagrabber import PatternDataladDataGrabber
 
 from ..api.decorators import register_datagrabber
-from ..utils import raise_error
-from .pattern import PatternDataGrabber
+# from ..utils import raise_error
+# from .pattern import PatternDataGrabber
 
 
 @register_datagrabber
-class AOMIC1000(PatternDataGrabber):
+class DataladAOMIC1000(PatternDataladDataGrabber):
     """Concrete implementation for pattern-based data fetching of AOMIC1000.
 
     Parameters
@@ -32,125 +32,127 @@ class AOMIC1000(PatternDataGrabber):
     def __init__(
         self,
         datadir: Union[str, Path, None] = None,
-        tasks: Union[str, List[str], None] = None,
+        # tasks: Union[str, List[str], None] = None,
         **kwargs,
     ) -> None:
-        """Initialize the class."""
-        # All tasks
-        all_tasks = [
-            "moviewatching",
-        ]
-        # Set default tasks
-        if tasks is None:
-            self.tasks: List[str] = all_tasks
-        # Convert single task into list
-        else:
-            if not isinstance(tasks, List):
-                tasks = [tasks]
-            # Check for invalid task(s)
-            for task in tasks:
-                if task not in all_tasks:
-                    raise_error(
-                        f"'{task}' is not a valid AOMIC fMRI task input. "
-                        f"Valid task values can be {all_tasks}."
-                    )
-            self.tasks: List[str] = tasks
+        # """Initialize the class."""
+        # # All tasks
+        # all_tasks = [
+        #     "moviewatching",
+        # ]
+        # # Set default tasks
+        # if tasks is None:
+        #     self.tasks: List[str] = all_tasks
+        # # Convert single task into list
+        # else:
+        #     if not isinstance(tasks, List):
+        #         tasks = [tasks]
+        #     # Check for invalid task(s)
+        #     for task in tasks:
+        #         if task not in all_tasks:
+        #             raise_error(
+        #                 f"'{task}' is not a valid AOMIC fMRI task input. "
+        #                 f"Valid task values can be {all_tasks}."
+        #             )
+        #     self.tasks: List[str] = tasks
 
         # The types of data
-        types = ["BOLD"]
+        types = [
+            "BOLD",
+            "BOLD_confounds",
+            "ANAT",
+            "DWI",
+        ]
         # The patterns
         patterns = {
             "BOLD": (
                 "derivatives/fmriprep/sub-{subject}/func/"
-                "sub-{subject}_task-{tasks}_"
+                "sub-{subject}_task-moviewatching_"
                 "space-MNI152NLin2009cAsym_desc-preproc_bold.nii.gz"
-            )
+            ),
+            "BOLD_confounds": (
+                "derivatives/fmriprep/sub-{subject}/func/"
+                "sub-{subject}_task-moviewatching_"
+                "desc-confounds_regressors.tsv"
+            ),
+            "ANAT": (
+                "derivatives/fmriprep/sub-{subject}/anat/"
+                "sub-{subject}_space-MNI152NLin2009cAsym_"
+                "desc-preproc_T1w.nii.gz"
+            ),
+            "DWI": (
+                "derivatives/dwipreproc/sub-{subject}/dwi/"
+                "sub-{subject}_desc-preproc_dwi.nii.gz"
+            ),
         }
         # The replacements
-        replacements = ["subject", "tasks"]
+        replacements = ["subject"]
+        uri = "https://github.com/OpenNeuroDatasets/ds003097.git"
         super().__init__(
             types=types,
             datadir=datadir,
+            uri=uri,
             patterns=patterns,
             replacements=replacements,
         )
 
-    def __getitem__(self, element: Tuple[str, str, str]) -> Dict[str, Path]:
-        """Index one element in the dataset.
+    # def __getitem__(self, element: Tuple[str, str, str]) -> Dict[str, Path]:
+    #     """Index one element in the dataset.
 
-        Parameters
-        ----------
-        element : triple of str
-            The element to be indexed. First element in the tuple is the
-            subject, second element is the task.
+    #     Parameters
+    #     ----------
+    #     element : triple of str
+    #         The element to be indexed. First element in the tuple is the
+    #         subject, second element is the task.
 
-        Returns
-        -------
-        out : dict
-            Dictionary of paths for each type of data required for the
-            specified element.
+    #     Returns
+    #     -------
+    #     out : dict
+    #         Dictionary of paths for each type of data required for the
+    #         specified element.
 
-        """
-        sub, task = element
+    #     """
+    #     sub, task = element
 
-        out = super().__getitem__((sub, task))
-        out["meta"]["element"] = {
-            "subject": sub,
-            "task": task,
-        }
-        return out
+    #     out = super().__getitem__((sub, task))
+    #     out["meta"]["element"] = {
+    #         "subject": sub,
+    #         # "task": task,
+    #     }
+    #     return out
 
-    def get_elements(self) -> List:
-        """Implement fetching list of subjects in the dataset.
+# @register_datagrabber
+# class DataladAOMIC1000(DataladDataGrabber, AOMIC1000):
+#     """Concrete implementation for datalad-based data fetching of AOMIC1000.
 
-        Returns
-        -------
-        elements : list of str
-            The list of subjects in the dataset.
+#     Parameters
+#     ----------
+#     datadir : str or Path, optional
+#         The directory where the datalad dataset will be cloned. If None,
+#         the datalad dataset will be cloned into a temporary directory
+#         (default None).
+#     tasks : {"moviewatching"} AOMIC 1000 tasks.
+#         If None, moviewatching is selected.
+#         (default None).
+#     **kwargs
+#         Keyword arguments passed to superclass.
 
-        """
-        subjects = [x.name for x in self.datadir.iterdir() if x.is_dir()]
-        elems = []
-        for subject, task in product(
-            subjects, self.tasks
-        ):
-            elems.append((subject, task))
+#     """
 
-        return elems
-
-
-@register_datagrabber
-class DataladAOMIC1000(DataladDataGrabber, AOMIC1000):
-    """Concrete implementation for datalad-based data fetching of AOMIC1000.
-
-    Parameters
-    ----------
-    datadir : str or Path, optional
-        The directory where the datalad dataset will be cloned. If None,
-        the datalad dataset will be cloned into a temporary directory
-        (default None).
-    tasks : {"moviewatching"} AOMIC 1000 tasks.
-        If None, moviewatching is selected.
-        (default None).
-    **kwargs
-        Keyword arguments passed to superclass.
-
-    """
-
-    def __init__(
-        self,
-        datadir: Union[str, Path, None] = None,
-        tasks: Union[str, List[str], None] = None,
-        **kwargs,
-    ) -> None:
-        """Initialize the class."""
-        uri = (
-            "https://github.com/OpenNeuroDatasets/ds003097.git"
-        )
-        rootdir = "derivatives"
-        super().__init__(
-            datadir=datadir,
-            tasks=tasks,
-            uri=uri,
-            rootdir=rootdir,
-        )
+#     def __init__(
+#         self,
+#         datadir: Union[str, Path, None] = None,
+#         tasks: Union[str, List[str], None] = None,
+#         **kwargs,
+#     ) -> None:
+#         """Initialize the class."""
+#         uri = (
+#             "https://github.com/OpenNeuroDatasets/ds003097.git"
+#         )
+#         rootdir = "derivatives"
+#         super().__init__(
+#             datadir=datadir,
+#             tasks=tasks,
+#             uri=uri,
+#             rootdir=rootdir,
+#         )
