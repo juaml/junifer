4cbee65250c36a4674254aaca50b143937da4378
LeSasse <leonard.sasse@hhu.de>
2022-10-06 09:58:21 +0200

    fix datagrabber/__init__.py

--- junifer/datagrabber/__init__.py
+++ junifer/datagrabber/__init__.py
@@ -10,3 +10,4 @@ from .hcp import DataladHCP1200, HCP1200
 from .multiple import MultipleDataGrabber
 from .pattern import PatternDataGrabber
 from .pattern_datalad import PatternDataladDataGrabber
+from .aomic import DataladAOMICID1000, DataladAOMICPIOP1, DataladAOMICPIOP2
