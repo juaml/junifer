c90ded7c5d4c7e4890073d16ddea55da9f9f3f21
LeSasse <leonard.sasse@hhu.de>
2022-10-10 11:59:43 +0200

    technically this should work but it doesnt

--- junifer/configs/juseless/datagrabbers/camcan_vbm.py
+++ junifer/configs/juseless/datagrabbers/camcan_vbm.py
@@ -29,28 +29,16 @@ class JuselessDataladCamCANVBM(PatternDataladDataGrabber):
 
     def __init__(self, datadir: Union[str, Path, None] = None) -> None:
         """Initialize the class."""
-        uri = "git@jugit.fz-juelich.de:inm7/datasets/datasets_repo.git"
+        uri = (
+            "http://cat_12.5.ds.inm7.de/a13/9b26a-8406-11ea-8f94-a0369f287950"
+        )
         types = ["VBM_GM"]
-        rootdir = "processed/cat_12.5/CamCAN"
         replacements = ["subject"]
         patterns = {"VBM_GM": "sub-{subject}/mri/m0wp1sub-{subject}.nii.gz"}
         super().__init__(
             types=types,
             datadir=datadir,
             uri=uri,
-            rootdir=rootdir,
             replacements=replacements,
             patterns=patterns,
         )
-
-    def get_elements(self) -> List:
-        """Implement fetching list of subjects in the dataset.
-
-        Returns
-        -------
-        elements : list of str
-            The list of subjects in the dataset.
-
-        """
-        self._dataset.get(self.datadir, get_data=False, source="inm7-storage")
-        return [x.as_posix().split("-")[1] for x in self.datadir.glob("sub-*")]
