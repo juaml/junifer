0ed1567dec1337e5d5728f2dbe767cf91bfa4f28
LeSasse <leonard.sasse@hhu.de>
2022-10-12 10:36:12 +0200

    test_utils confused pytest

--- junifer/datagrabber/tests/test_datagrabber_utils.py
+++ junifer/datagrabber/tests/test_datagrabber_utils.py
@@ -6,6 +6,7 @@
 import pytest
 
 from junifer.datagrabber.utils import (
+    validate_and_format_parameters,
     validate_patterns,
     validate_replacements,
     validate_types,
@@ -86,3 +87,48 @@ def test_validate_patterns() -> None:
         validate_patterns(types, wrongpatterns)
 
     validate_patterns(types, patterns)
+
+
+def test_validate_and_format_none():
+    """Test validate_and_format_parameters if None."""
+
+    valid_params = ["p1", "p2", "p3"]
+    validated = validate_and_format_parameters(
+        None,
+        valid_params,
+        "Invalid parameter, valid_parameters are {valid_params}",
+    )
+    assert valid_params == validated
+
+
+def test_validate_and_format_str():
+    """Test validate_and_format_parameters if str."""
+    valid_params = ["p1", "p2", "p3"]
+    validated = validate_and_format_parameters(
+        "p1",
+        valid_params,
+        "Invalid parameter, valid_parameters are {valid_params}",
+    )
+    assert ["p1"] == validated
+
+
+def test_validate_and_format_list():
+    """Test validate_and_format_parameters if list."""
+    valid_params = ["p1", "p2", "p3"]
+    validated = validate_and_format_parameters(
+        ["p1", "p2"],
+        valid_params,
+        "Invalid parameter, valid_parameters are {valid_params}",
+    )
+    assert ["p1", "p2"] == validated
+
+
+def test_validate_and_format_invalid():
+    """Test validate_and_format_parameters if invalid."""
+    with pytest.raises(ValueError, match="Invalid parameter, "):
+        valid_params = ["p1", "p2", "p3"]
+        validate_and_format_parameters(
+            "invalid",
+            valid_params,
+            "Invalid parameter, valid_parameters are {valid_params}",
+        )
