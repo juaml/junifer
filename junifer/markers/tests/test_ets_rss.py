4aae546f0a5f931d04bf52e77d95e61c8d02d68c
Synchon Mandal <synchon@protonmail.com>
2022-10-13 19:35:11 +0200

    update: improve and complete testing for RSSETSMarker

--- junifer/markers/tests/test_ets_rss.py
+++ junifer/markers/tests/test_ets_rss.py
@@ -14,26 +14,35 @@ from nilearn.maskers import NiftiLabelsMasker
 from junifer.data import load_atlas
 from junifer.markers.ets_rss import RSSETSMarker
 from junifer.testing.datagrabbers import SPMAuditoryTestingDatagrabber
+from junifer.storage import SQLiteFeatureStorage
+
+
+# Set atlas
+ATLAS = "Schaefer100x17"
 
 
 def test_compute() -> None:
-    """Test RSS ETS."""
-    atlas = "Schaefer100x17"
-    test_atlas, _, _ = load_atlas(atlas)
-
+    """Test RSS ETS compute()."""
     with SPMAuditoryTestingDatagrabber() as dg:
+        # Fetch element
         out = dg["sub001"]
+        # Load BOLD image
         niimg = image.load_img(str(out["BOLD"]["path"].absolute()))
+        # Create input data
         input_dict = {"data": niimg, "path": out["BOLD"]["path"]}
         # Compute the RSSETSMarker
-        ets_rss_marker = RSSETSMarker(atlas=atlas)
+        ets_rss_marker = RSSETSMarker(atlas=ATLAS)
         new_out = ets_rss_marker.compute(input_dict)
+
+        # Load atlas
+        test_atlas, _, _ = load_atlas(ATLAS)
         # Compute the NiftiLabelsMasker
         test_masker = NiftiLabelsMasker(test_atlas)
         test_ts = test_masker.fit_transform(niimg)
         # Assert the dimension of timeseries
         n_time, _ = test_ts.shape
         assert n_time == len(new_out["data"])
+
         # Assert the meta
         meta = ets_rss_marker.get_meta("BOLD")["marker"]
         assert meta["atlas"] == "Schaefer100x17"
@@ -42,10 +51,8 @@ def test_compute() -> None:
 
 
 def test_get_output_kind() -> None:
-    """Test get_output_kind."""
-
-    atlas = "Schaefer100x17"
-    ets_rss_marker = RSSETSMarker(atlas=atlas)
+    """Test RSS ETS get_output_kind()."""
+    ets_rss_marker = RSSETSMarker(atlas=ATLAS)
     input_list = ["BOLD"]
     input_list = ets_rss_marker.get_output_kind(input_list)
     assert len(input_list) == 1
@@ -53,19 +60,26 @@ def test_get_output_kind() -> None:
 
 
 def test_store(tmp_path: Path) -> None:
-    """Test store."""
+    """Test RSS ETS store().
 
-    atlas = "Schaefer100x17"
+    Parameters
+    ----------
+    tmp_path : pathlib.Path
+        The path to the test directory.
+
+    """
     with SPMAuditoryTestingDatagrabber() as dg:
+        # Fetch element
         out = dg["sub001"]
+        # Load BOLD image
         niimg = image.load_img(str(out["BOLD"]["path"].absolute()))
         input_dict = {"data": niimg, "path": out["BOLD"]["path"]}
         # Compute the RSSETSMarker
-        ets_rss_marker = RSSETSMarker(atlas=atlas)
-        _ = ets_rss_marker.compute(input_dict)
-        # TODO: Needs store_timeseries implemented for SQLiteFeatureStorage
-        # storage = {
-        #     "kind": "SQLiteFeatureStorage",
-        #     "uri": str((tmp_path / "test.db").absolute()),
-        # }
-        # ets_rss_marker.store("SQLiteFeatureStorage", new_out, storage)
+        ets_rss_marker = RSSETSMarker(atlas=ATLAS)
+        # Create storage
+        storage = SQLiteFeatureStorage(
+            uri=str((tmp_path / "test.db").absolute()),
+            single_output=True,
+        )
+        # Store
+        ets_rss_marker.fit_transform(input=input_dict, storage=storage)
