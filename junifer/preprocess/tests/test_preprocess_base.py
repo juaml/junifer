31ba3c165d1dc83e6efc716259df90bc426b13ea
Fede <fraimondo@proton.me>
2022-10-30 11:51:20 +0200

    One more test

---
+++ junifer/preprocess/tests/test_preprocess_base.py
@@ -0,0 +1,52 @@
+"""Provide tests for base marker."""
+
+# Authors: Federico Raimondo <f.raimondo@fz-juelich.de>
+#          Synchon Mandal <s.mandal@fz-juelich.de>
+# License: AGPL
+
+import pytest
+
+from junifer.preprocess.base import BasePreprocessor
+
+
+def test_base_preprocessor_abstractness() -> None:
+    """Test BasePreprocessor is abstract base class."""
+    with pytest.raises(TypeError, match=r"abstract"):
+        BasePreprocessor(on=["BOLD"])  # type: ignore
+
+
+def test_base_preprocessor_subclassing() -> None:
+    """Test proper subclassing of BasePreprocessor."""
+    # Create concrete class
+    class MyBasePreprocessor(BasePreprocessor):
+        def get_valid_inputs(self):
+            return ["BOLD", "T1w"]
+
+        def get_output_kind(self, input):
+            return ["timeseries"]
+
+        def preprocess(self, input, extra_input):
+            return "BOLD", input
+
+    with pytest.raises(ValueError, match=r"cannot be computed on \['T2w'\]"):
+        MyBasePreprocessor(on=["BOLD", "T2w"])
+
+    # Create input for marker
+    input_ = {
+        "meta": {"datagrabber": "dg", "element": "elem", "datareader": "dr"},
+        "BOLD": {
+            "path": ".",
+            "data": "data",
+        },
+    }
+    prep = MyBasePreprocessor(on=["BOLD"])
+
+    with pytest.raises(ValueError, match="not have the required data"):
+        prep.validate_input(["T1w"])
+
+    output = prep.fit_transform(input=input_)  # process
+    # Check output
+    assert "BOLD" in output
+    assert "data" in output["BOLD"]
+    assert "path" in output["BOLD"]
+    assert "meta" in output["BOLD"]
