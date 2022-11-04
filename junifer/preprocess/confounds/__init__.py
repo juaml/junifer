31d3402c8bef660bf4f22c00daeccda9243722a3
Fede <fraimondo@proton.me>
2022-11-04 11:02:03 +0200

    @synchon: comments addressed

---
+++ junifer/preprocess/confounds/__init__.py
@@ -0,0 +1,7 @@
+"""Provide imports for confounds sub-package."""
+
+# Authors: Federico Raimondo <f.raimondo@fz-juelich.de>
+#          Leonard Sasse <l.sasse@fz-juelich.de>
+# License: AGPL
+
+from .fmriprep_confound_remover import fMRIPrepConfoundRemover
--- junifer/testing/utils.py
+++ junifer/testing/utils.py
@@ -6,9 +6,20 @@
 from pathlib import Path
 
 
-def get_testing_data(fname):
-    """Get the path to a testing data file."""
+def get_testing_data(fname : str) -> Path:
+    """Get the path to a testing data file.
+
+    Parameters
+    ----------
+    fname : str
+        The name of the file.
+
+    Returns
+    -------
+    pathlib.Path
+        The absolute path to the file.
+    """
     t_path = Path(__file__).parent / "data" / fname
     if not t_path.exists():
         raise FileNotFoundError(f"File {fname} not found in testing data")
-    return t_path
+    return t_path.resolve()
