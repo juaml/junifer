109b2142f079001648939322a7f20bde0defa0e9
Fede <fraimondo@proton.me>
2022-11-04 09:04:19 +0200

    lint

--- junifer/testing/utils.py
+++ junifer/testing/utils.py
@@ -5,9 +5,10 @@
 
 from pathlib import Path
 
+
 def get_testing_data(fname):
     """Get the path to a testing data file."""
-    t_path = Path(__file__).parent / 'data' / fname
+    t_path = Path(__file__).parent / "data" / fname
     if not t_path.exists():
         raise FileNotFoundError(f"File {fname} not found in testing data")
     return t_path
