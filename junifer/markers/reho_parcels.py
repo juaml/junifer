e4b992018a5344a721ee1211ada9f12fe58ec5a4
Synchon Mandal <synchon@protonmail.com>
2022-11-24 15:10:49 +0100

    update: use in-place processing for parcel aggregation output in reho parcels

--- junifer/markers/reho_parcels.py
+++ junifer/markers/reho_parcels.py
@@ -136,15 +136,13 @@ class ReHoParcels(BaseMarker):
             on="BOLD",
         )
         # Perform aggregation
-        aggregated_values = parcel_aggregation.compute(
+        output = parcel_aggregation.compute(
             input=input, extra_input=extra_input
         )
-        # Create a new dictionary for returning
-        output = {}
         # Expand row dimension
-        output["data"] = np.expand_dims(aggregated_values["data"], axis=0)
-        # Set column labels
-        output["columns"] = aggregated_values["columns"]
+        output["data"] = output["data"][np.newaxis, :]
+        # Delete row_names
+        del output["row_names"]
         # Set row_cols_name to None
         output["rows_col_name"] = None
         return output
