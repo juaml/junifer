8dddbc4d25107b14f9798eabab507da9069bb037
Synchon Mandal <synchon@protonmail.com>
2022-11-28 19:07:10 +0100

    update: add check for importable dependencies in PipelineStepMixin.validate()

--- junifer/pipeline/pipeline_step_mixin.py
+++ junifer/pipeline/pipeline_step_mixin.py
@@ -4,6 +4,7 @@
 #          Synchon Mandal <s.mandal@fz-juelich.de>
 # License: AGPL
 
+from importlib.util import find_spec
 from typing import Dict, List
 
 from ..utils import raise_error
@@ -67,9 +68,27 @@ class PipelineStepMixin:
         Raises
         ------
         ValueError
-            If the input does not have the required data.
+            If the pipeline step object is missing dependencies required for
+            its working or if the input does not have the required data.
 
         """
+        # Check if _DEPENDENCIES attribute is found;
+        # (markers and preprocessors will have them but not datareaders
+        # as of now)
+        dependencies_not_found = []
+        if hasattr(self, "_DEPENDENCIES"):
+            # Check if dependencies are importable
+            for dependency in getattr(self, "_DEPENDENCIES"):
+                if find_spec(dependency) is None:
+                    dependencies_not_found.append(dependency)
+        # Raise error if any dependency is not found
+        if dependencies_not_found:
+            raise_error(
+                msg=f"{dependencies_not_found} are not installed but are "
+                "required for using {self.name}.",
+                klass=ImportError,
+            )
+
         self.validate_input(input=input)
         outputs = [self.get_output_type(t_input) for t_input in input]
         return outputs
