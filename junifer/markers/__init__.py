be11fed73e855a77a8d0e310cce2add4bca25ad9
Synchon Mandal <synchon@protonmail.com>
2022-11-29 18:31:52 +0100

    refactor: move ReHoParcels() and ReHoSpheres() to reho sub-package

--- junifer/markers/__init__.py
+++ junifer/markers/__init__.py
@@ -12,5 +12,4 @@ from .functional_connectivity_parcels import FunctionalConnectivityParcels
 from .functional_connectivity_spheres import FunctionalConnectivitySpheres
 from .parcel_aggregation import ParcelAggregation
 from .sphere_aggregation import SphereAggregation
-from .reho_parcels import ReHoParcels
-from .reho_spheres import ReHoSpheres
+from .reho import ReHoParcels, ReHoSpheres
