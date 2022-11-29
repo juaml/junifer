90712d8c646abc023d67f90c2bff74f0201797e0
Synchon Mandal <synchon@protonmail.com>
2022-11-29 19:30:38 +0100

    update: add singleton decorator function for markers in markers.utils

--- junifer/markers/utils.py
+++ junifer/markers/utils.py
@@ -4,15 +4,55 @@
 #          Nicolás Nieto <n.nieto@fz-juelich.de>
 #          Sami Hamdan <s.hamdan@fz-juelich.de>
 #          Synchon Mandal <s.mandal@fz-juelich.de>
+#          Federico Raimondo <f.raimondo@fz-juelich.de>
 # License: AGPL
 
-from typing import Callable, Union
+from typing import Any, Callable, Dict, Type, Union
 
 import numpy as np
 import pandas as pd
 from scipy.stats import zscore
 
 from ..utils import raise_error
+
+
+def singleton(cls: Type) -> Type:
+    """Make a class singleton.
+
+    Parameters
+    ----------
+    cls : class
+        The class to designate as singleton.
+
+    Returns
+    -------
+    class
+        The only instance of the class.
+
+    """ ""
+    instances: Dict = {}
+
+    def get_instance(*args: Any, **kwargs: Any) -> Type:
+        """Get the only instance for a class.
+
+        Parameters
+        ----------
+        *args : tuple
+            The positional arguments to pass to the class.
+        **kwargs : dict
+            The keyword arguments to pass to the class.
+
+        Returns
+        -------
+        class
+            The only instance of the class.
+
+        """ ""
+        if cls not in instances:
+            instances[cls] = cls(*args, **kwargs)
+        return instances[cls]
+
+    return get_instance
 
 
 def _ets(bold_ts: np.ndarray) -> np.ndarray:
