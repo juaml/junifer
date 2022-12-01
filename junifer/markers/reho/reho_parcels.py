bddb86e0a993c0c20b6d6afa498f322f3b55c91a
Synchon Mandal <synchon@protonmail.com>
2022-12-01 11:01:16 +0100

    update: make ReHoParcels() a subclass of ReHoBase()

--- junifer/markers/reho/reho_parcels.py
+++ junifer/markers/reho/reho_parcels.py
@@ -4,22 +4,16 @@
 # License: AGPL
 
 
-from typing import TYPE_CHECKING, Any, Dict, List, Optional
-
-import numpy as np
+from typing import Any, Dict, Optional
 
 from ...api.decorators import register_marker
 from ...utils import logger
-from ..base import BaseMarker
 from ..parcel_aggregation import ParcelAggregation
-
-
-if TYPE_CHECKING:
-    from junifer.storage import BaseFeatureStorage
+from .reho_base import ReHoBase
 
 
 @register_marker
-class ReHoParcels(BaseMarker):
+class ReHoParcels(ReHoBase):
     """Class for regional homogeneity on parcels.
 
     Parameters
@@ -27,6 +21,18 @@ class ReHoParcels(BaseMarker):
     parcellation : str
         The name of the parcellation. Check valid options by calling
         :func:`junifer.data.parcellations.list_parcellations`.
+    reho_params : dict, optional
+        Extra parameters for computing ReHo map as a dictionary (default None).
+    agg_method : str, optional
+        The method to perform aggregation using. Check valid options in
+        :func:`junifer.stats.get_aggfunc_by_name` (default "mean").
+    agg_method_params : dict, optional
+        Parameters to pass to the aggregation function. Check valid options in
+        :func:`junifer.stats.get_aggfunc_by_name` (default None).
+    mask : str, optional
+        The name of the mask to apply to regions before extracting signals.
+        Check valid options by calling :func:`junifer.data.masks.list_masks`
+        (default None).
     name : str, optional
         The name of the marker. If None, it will use the class name
         (default None).
@@ -36,59 +42,18 @@ class ReHoParcels(BaseMarker):
     def __init__(
         self,
         parcellation: str,
+        reho_params: Optional[Dict] = None,
+        agg_method: str = "mean",
+        agg_method_params: Optional[Dict] = None,
+        mask: Optional[str] = None,
         name: Optional[str] = None,
     ) -> None:
         self.parcellation = parcellation
+        self.reho_params = reho_params
+        self.agg_method = agg_method
+        self.agg_method_params = agg_method_params
+        self.mask = mask
         super().__init__(name=name)
-
-    def get_valid_inputs(self) -> List[str]:
-        """Get valid data types for input.
-
-        Returns
-        -------
-        list of str
-            The list of data types that can be used as input for this marker.
-
-        """
-        return ["BOLD"]
-
-    def get_output_kind(self, input: List[str]) -> List[str]:
-        """Get output kind.
-
-        Parameters
-        ----------
-        input : list of str
-            The input to the marker. The list must contain the
-            available Junifer Data dictionary keys.
-
-        Returns
-        -------
-        list of str
-            The updated list of output kinds, as storage possibilities.
-
-        """
-        return ["table"]
-
-    def store(
-        self,
-        kind: str,
-        out: Dict[str, Any],
-        storage: "BaseFeatureStorage",
-    ) -> None:
-        """Store.
-
-        Parameters
-        ----------
-        kind : {"BOLD"}
-            The data kind to store.
-        out : dict
-            The computed result as a dictionary to store.
-        storage : storage-like
-            The storage class, for example, SQLiteFeatureStorage.
-
-        """
-        logger.debug(f"Storing {kind} in {storage}")
-        storage.store(kind="table", **out)
 
     def compute(
         self,
@@ -96,11 +61,6 @@ class ReHoParcels(BaseMarker):
         extra_input: Optional[Dict[str, Any]] = None,
     ) -> Dict[str, Any]:
         """Compute.
-
-        For a given voxel, identifies the set of neighbours within a certain
-        radius and then calculates Kendall's W for the voxel and its
-        neighbours for the timepoints in the BOLD signal. For more
-        information about the method, please check [1]_.
 
         Parameters
         ----------
@@ -119,30 +79,28 @@ class ReHoParcels(BaseMarker):
             * ``columns`` : the column labels for the parcels as a list
             * ``row_names`` : ``None``
 
-        References
-        ----------
-        .. [1] Jiang, L., & Zuo, X. N. (2016).
-               Regional Homogeneity: A Multimodal, Multiscale Neuroimaging
-               Marker of the Human Connectome.
-               The Neuroscientist, Volume 22(5), Pages 486–505.
-               https://doi.org/10.1177/1073858415595004
-
         """
-        logger.debug("Calculating ReHO for parcels.")
+        logger.info("Calculating ReHO for parcels.")
+        # Calculate reho map
+        if self.reho_params is not None:
+            reho_map = self.compute_reho_map(input=input, **self.reho_params)
+        else:
+            reho_map = self.compute_reho_map(input=input)
         # Initialize parcel aggregation
         parcel_aggregation = ParcelAggregation(
             parcellation=self.parcellation,
-            method="kendall_w",
+            method=self.agg_method,
+            method_params=self.agg_method_params,
+            mask=self.mask,
             on="BOLD",
         )
-        # Perform aggregation
-        output = parcel_aggregation.compute(
-            input=input, extra_input=extra_input
-        )
-        # Expand row dimension
-        output["data"] = output["data"][np.newaxis, :]
-        # Delete row_names
-        del output["row_names"]
-        # Set row_cols_name to None
-        output["rows_col_name"] = None
+        # Perform aggregation on reho map
+        parcel_aggregation_input = {"data": reho_map}
+        output = parcel_aggregation.compute(input=parcel_aggregation_input)
+        # # Expand row dimension
+        # output["data"] = output["data"][np.newaxis, :]
+        # # Delete row_names
+        # del output["row_names"]
+        # # Set row_cols_name to None
+        # output["rows_col_name"] = None
         return output
