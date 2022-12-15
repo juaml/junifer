c5d6bf1a34c47a1835636636d363283f64abfcb3
Synchon Mandal <synchon@protonmail.com>
2022-12-15 16:40:34 +0100

    update: add test for comparing ReHoParcels' implementation

--- junifer/markers/reho/tests/test_reho_parcels.py
+++ junifer/markers/reho/tests/test_reho_parcels.py
@@ -5,9 +5,12 @@
 
 from pathlib import Path
 
+import pytest
 from nilearn import image as nimg
+from scipy.stats import pearsonr
 
 from junifer.markers.reho.reho_parcels import ReHoParcels
+from junifer.pipeline.utils import _check_afni
 from junifer.storage.sqlite import SQLiteFeatureStorage
 from junifer.testing.datagrabbers import SPMAuditoryTestingDatagrabber
 
@@ -33,7 +36,49 @@ def test_reho_parcels_computation() -> None:
         # Assert BOLD output
         assert "data" in reho_parcels_output_bold
         assert "columns" in reho_parcels_output_bold
-        # TODO: add comparison with afni 3dReHo generated data
+
+
+@pytest.mark.skipif(
+    _check_afni() is False, reason="requires afni to be in PATH"
+)
+def test_reho_parcels_computation_comparison() -> None:
+    """Test ReHoParcels fit-transform implementation comparison.."""
+    with SPMAuditoryTestingDatagrabber() as dg:
+        # Use first subject
+        subject_data = dg["sub001"]
+        # Load image to memory
+        fmri_img = nimg.load_img(subject_data["BOLD"]["path"])
+
+        # Initialize marker with use_afni=False
+        reho_parcels_marker_python = ReHoParcels(
+            parcellation=PARCELLATION, use_afni=False
+        )
+        # Fit transform marker on data
+        reho_parcels_output_python = reho_parcels_marker_python.fit_transform(
+            {"BOLD": {"path": "/tmp", "data": fmri_img, "meta": {}}}
+        )
+        # Get BOLD output
+        reho_parcels_output_bold_python = reho_parcels_output_python["BOLD"]
+
+        # Initialize marker with use_afni=True
+        reho_parcels_marker_afni = ReHoParcels(
+            parcellation=PARCELLATION, use_afni=True
+        )
+        # Fit transform marker on data
+        reho_parcels_output_afni = reho_parcels_marker_afni.fit_transform(
+            {"BOLD": {"path": "/tmp", "data": fmri_img, "meta": {}}}
+        )
+        # Get BOLD output
+        reho_parcels_output_bold_afni = reho_parcels_output_afni["BOLD"]
+
+        # Check for Pearson correlation coefficient
+        assert (
+            pearsonr(
+                reho_parcels_output_bold_python["data"].flatten(),
+                reho_parcels_output_bold_afni["data"].flatten(),
+            ).statistic
+            >= 0.9
+        )
 
 
 def test_reho_parcels_storage(tmp_path: Path) -> None:
