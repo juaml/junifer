be55e85753d8a2f7191448b5d87b317103d5709d
Synchon Mandal <synchon@protonmail.com>
2023-01-03 13:40:03 +0100

    update: add imports for markers.functional_connectivity

---
+++ junifer/markers/functional_connectivity/__init__.py
@@ -0,0 +1,7 @@
+"""Provide imports for functional connectivity sub-package."""
+
+# Authors: Synchon Mandal <s.mandal@fz-juelich.de>
+# License: AGPL
+
+from .functional_connectivity_parcels import FunctionalConnectivityParcels
+from .functional_connectivity_spheres import FunctionalConnectivitySpheres
