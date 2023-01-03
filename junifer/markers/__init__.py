8fd43de26b9f5d74dbff883d2b6e15160a11dc8a
Synchon Mandal <synchon@protonmail.com>
2023-01-03 13:48:00 +0100

    update: correct imports for junifer.markers

--- junifer/markers/__init__.py
+++ junifer/markers/__init__.py
@@ -2,16 +2,19 @@
 
 # Authors: Federico Raimondo <f.raimondo@fz-juelich.de>
 #          Leonard Sasse <l.sasse@fz-juelich.de>
+#          Synchon Mandal <s.mandal@fz-juelich.de>
 # License: AGPL
 
 from .base import BaseMarker
 from .collection import MarkerCollection
 from .crossparcellation_functional_connectivity import CrossParcellationFC
 from .ets_rss import RSSETSMarker
-from .functional_connectivity_parcels import FunctionalConnectivityParcels
-from .functional_connectivity_spheres import FunctionalConnectivitySpheres
 from .parcel_aggregation import ParcelAggregation
 from .sphere_aggregation import SphereAggregation
+from .functional_connectivity import (
+    FunctionalConnectivityParcels,
+    FunctionalConnectivitySpheres,
+)
 from .reho import ReHoParcels, ReHoSpheres
 from .falff import (
     AmplitudeLowFrequencyFluctuationParcels,
