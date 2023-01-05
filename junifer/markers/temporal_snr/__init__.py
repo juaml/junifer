c1e256c2711ccaabfc73bfb3fdb435a39ecb5c54
LeSasse <leonard.sasse@hhu.de>
2023-01-05 15:20:14 +0100

    add imports to __init__.py

---
+++ junifer/markers/temporal_snr/__init__.py
@@ -0,0 +1,7 @@
+"""Provide imports for temporal signal-to-noise ratio sub-package."""
+
+# Authors: Leonard Sasse <l.sasse@fz-juelich.de>
+# License: AGPL
+
+from .temporal_snr_parcels import TemporalSNRParcels
+from .temporal_snr_spheres import TemporalSNRSpheres
