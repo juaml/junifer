21430fc765a7a709f26fd67c708c33db55000c2f
Synchon Mandal <synchon@protonmail.com>
2023-01-17 15:57:11 +0100

    update: add imports for markers.complexity.__init__

--- junifer/markers/complexity/__init__.py
+++ junifer/markers/complexity/__init__.py
@@ -4,3 +4,9 @@
 # License: AGPL
 
 from .hurst_exponent import HurstExponent
+from .range_entropy import RangeEntropy
+from .range_entropy_auc import RangeEntropyAUC
+from .perm_entropy import PermEntropy
+from .weighted_perm_entropy import WeightedPermEntropy
+from .sample_entropy import SampleEntropy
+from .multiscale_entropy_auc import MultiscaleEntropyAUC
