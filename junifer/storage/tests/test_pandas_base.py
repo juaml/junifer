4acb42c160dbfc8594dff4551129cff55d16cdfc
Synchon Mandal <synchon@protonmail.com>
2023-02-01 13:19:35 +0100

    update: fix tests for PandasBaseFeatureStorage.element_to_index()

--- junifer/storage/tests/test_pandas_base.py
+++ junifer/storage/tests/test_pandas_base.py
@@ -9,62 +9,95 @@ from junifer.storage.pandas_base import PandasBaseFeatureStorage
 
 def test_element_to_index() -> None:
     """Test element to index."""
+    # First element
     element = {"foo": "bar"}
-    index = PandasBaseFeatureStorage.element_to_index(element)
-    assert index.names == ["foo", "idx"]
-    assert index.levels[0].name == "foo"
-    assert index.levels[0].values[0] == "bar"
-    assert all(x == "bar" for x in index.levels[0].values)
-    assert index.levels[0].values.shape == (1,)
-    assert index.levels[1].name == "idx"
-    assert all(x == i for i, x in enumerate(index.levels[1].values))
-    assert index.levels[1].values.shape == (1,)
 
-    index = PandasBaseFeatureStorage.element_to_index(element, n_rows=10)
-    assert index.names == ["foo", "idx"]
-    assert index.levels[0].name == "foo"
-    assert all(x == "bar" for x in index.levels[0].values)
-    assert index.levels[0].values.shape == (1,)
+    # First test; no extra column
+    index = PandasBaseFeatureStorage.element_to_index(element=element)
+    # Check index names
+    assert index.names == ["foo"]
+    # Check first index level name
+    assert index.levels[0].name == "foo"  # type: ignore
+    # Check first index level values
+    assert index.levels[0].values[0] == "bar"  # type: ignore
+    assert all(x == "bar" for x in index.levels[0].values)  # type: ignore
+    # Check first index level values shape
+    assert index.levels[0].values.shape == (1,)  # type: ignore
 
-    assert index.levels[1].name == "idx"
-    assert all(x == i for i, x in enumerate(index.levels[1].values))
-    assert index.levels[1].values.shape == (10,)
-
+    # Second test; add extra column
     index = PandasBaseFeatureStorage.element_to_index(
-        element, n_rows=1, rows_col_name="scan"
+        element=element, n_rows=10
     )
-    assert index.names == ["foo", "scan"]
-    assert index.levels[0].name == "foo"
-    assert index.levels[0].values[0] == "bar"
-    assert all(x == "bar" for x in index.levels[0].values)
-    assert index.levels[0].values.shape == (1,)
-    assert index.levels[1].name == "scan"
-    assert all(x == i for i, x in enumerate(index.levels[1].values))
-    assert index.levels[1].values.shape == (1,)
+    # Check index names
+    assert index.names == ["foo", "idx"]
+    # Check first index level name
+    assert index.levels[0].name == "foo"  # type: ignore
+    # Check first index level values
+    assert all(x == "bar" for x in index.levels[0].values)  # type: ignore
+    # Check first index level values shape
+    assert index.levels[0].values.shape == (1,)  # type: ignore
+    # Check second index level name
+    assert index.levels[1].name == "idx"  # type: ignore
+    # Check second index level values
+    assert all(x == i for i, x in enumerate(index.levels[1].values))  # type: ignore
+    # Check second index level values shape
+    assert index.levels[1].values.shape == (10,)  # type: ignore
 
+    # Third test; custom extra column name has no effect
     index = PandasBaseFeatureStorage.element_to_index(
-        element, n_rows=7, rows_col_name="scan"
+        element=element, n_rows=1, rows_col_name="scan"
     )
+    # Check index names
+    assert index.names == ["foo"]
+    # Check first index level name
+    assert index.levels[0].name == "foo"  # type: ignore
+    # Check first index level values
+    assert index.levels[0].values[0] == "bar"  # type: ignore
+    assert all(x == "bar" for x in index.levels[0].values)  # type: ignore
+    # Check first index level values shape
+    assert index.levels[0].values.shape == (1,)  # type: ignore
+
+    # Fourth test; custom extra column name has effect
+    index = PandasBaseFeatureStorage.element_to_index(
+        element=element, n_rows=7, rows_col_name="scan"
+    )
+    # Check index names
     assert index.names == ["foo", "scan"]
-    assert index.levels[0].name == "foo"
-    assert all(x == "bar" for x in index.levels[0].values)
-    assert index.levels[0].values.shape == (1,)
+    # Check first index level name
+    assert index.levels[0].name == "foo"  # type: ignore
+    # Check first index level values
+    assert all(x == "bar" for x in index.levels[0].values)  # type: ignore
+    # Check first index level values shape
+    assert index.levels[0].values.shape == (1,)  # type: ignore
+    # Check second index level name
+    assert index.levels[1].name == "scan"  # type: ignore
+    # Check second index level values
+    assert all(x == i for i, x in enumerate(index.levels[1].values))  # type: ignore
+    # Check second index level values shape
+    assert index.levels[1].values.shape == (7,)  # type: ignore
 
-    assert index.levels[1].name == "scan"
-    assert all(x == i for i, x in enumerate(index.levels[1].values))
-    assert index.levels[1].values.shape == (7,)
-
+    # Second element
     element = {"subject": "sub-01", "session": "ses-01"}
-    index = PandasBaseFeatureStorage.element_to_index(element, n_rows=10)
 
-    assert index.levels[0].name == "subject"
-    assert all(x == "sub-01" for x in index.levels[0].values)
-    assert index.levels[0].values.shape == (1,)
-
-    assert index.levels[1].name == "session"
-    assert all(x == "ses-01" for x in index.levels[1].values)
-    assert index.levels[1].values.shape == (1,)
-
-    assert index.levels[2].name == "idx"
-    assert all(x == i for i, x in enumerate(index.levels[2].values))
-    assert index.levels[2].values.shape == (10,)
+    # Fifth test; default name for extra column and multi-level element access
+    index = PandasBaseFeatureStorage.element_to_index(
+        element=element, n_rows=10
+    )
+    # Check first index level name
+    assert index.levels[0].name == "subject"  # type: ignore
+    # Check first index level values
+    assert all(x == "sub-01" for x in index.levels[0].values)  # type: ignore
+    # Check first index level values shape
+    assert index.levels[0].values.shape == (1,)  # type: ignore
+    # Check second index level name
+    assert index.levels[1].name == "session"  # type: ignore
+    # Check second index level values
+    assert all(x == "ses-01" for x in index.levels[1].values)  # type: ignore
+    # Check second index level values shape
+    assert index.levels[1].values.shape == (1,)  # type: ignore
+    # Check third index level name
+    assert index.levels[2].name == "idx"  # type: ignore
+    # Check third index level values
+    assert all(x == i for i, x in enumerate(index.levels[2].values))  # type: ignore
+    # Check third index level values shape
+    assert index.levels[2].values.shape == (10,)  # type: ignore
