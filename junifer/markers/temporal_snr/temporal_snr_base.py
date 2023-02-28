03b930125bf4b559aeb192b475344fb9dbbda07e
Synchon Mandal <synchon@protonmail.com>
2023-02-28 15:42:06 +0100

    update: rename table to vector in TemporalSNRBase

--- junifer/markers/temporal_snr/temporal_snr_base.py
+++ junifer/markers/temporal_snr/temporal_snr_base.py
@@ -79,7 +79,7 @@ class TemporalSNRBase(BaseMarker):
             The storage type output by the marker.
 
         """
-        return "table"
+        return "vector"
 
     def compute(
         self,
