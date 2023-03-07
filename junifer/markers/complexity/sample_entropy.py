54ed1e3344be914a7477bdeb65b090fe81a5d117
Synchon Mandal <synchon@protonmail.com>
2023-03-07 11:46:15 +0100

    refactor: move Sample Entropy computation to respective class

--- junifer/markers/complexity/sample_entropy.py
+++ junifer/markers/complexity/sample_entropy.py
@@ -4,16 +4,14 @@
 #          Leonard Sasse <l.sasse@fz-juelich.de>
 # License: AGPL
 
-from typing import TYPE_CHECKING, Dict, List, Optional, Union
+from typing import Dict, List, Optional, Union
+
+import neurokit2 as nk
+import numpy as np
 
 from ...api.decorators import register_marker
-from ...utils import logger
-from ..utils import _sample_entropy
+from ...utils import logger, warn_with_log
 from .complexity_base import ComplexityBase
-
-
-if TYPE_CHECKING:
-    import numpy as np
 
 
 @register_marker
@@ -70,11 +68,11 @@ class SampleEntropy(ComplexityBase):
 
     def compute_complexity(
         self,
-        extracted_bold_values: "np.ndarray",
-    ) -> "np.ndarray":
+        extracted_bold_values: np.ndarray,
+    ) -> np.ndarray:
         """Compute complexity measure.
 
-        Take a timeseries of brain areas, and calculate the sample entropy [1].
+        Take a timeseries of brain areas, and calculate sample entropy [1].
 
         Parameters
         ----------
@@ -94,8 +92,36 @@ class SampleEntropy(ComplexityBase):
                Am. J. Physiol. Heart Circ. Physiol., 278 (6) (2000),
                pp. H2039-2049
 
+        See also
+        --------
+        neurokit2.entropy_sample
+
         """
         logger.info("Calculating sample entropy.")
-        return _sample_entropy(
-            bold_ts=extracted_bold_values, params=self.params
-        )  # 1 X n_roi
+
+        emb_dim = self.params["m"]
+        delay = self.params["delay"]
+        tol = self.params["tol"]
+
+        assert isinstance(emb_dim, int), "Embedding dimension must be integer."
+        assert isinstance(delay, int), "Delay must be integer."
+        assert isinstance(
+            tol, float
+        ), "Tolerance must be a positive float number."
+
+        _, n_roi = extracted_bold_values.shape
+        samp_en_roi = np.zeros((n_roi, 1))
+
+        for idx_roi in range(n_roi):
+            sig = extracted_bold_values[:, idx_roi]
+            tol_corrected = tol * np.std(sig)
+            tmp = nk.entropy_sample(
+                sig, dimension=emb_dim, delay=delay, tolerance=tol_corrected
+            )
+
+            samp_en_roi[idx_roi] = tmp[0]
+
+        if np.isnan(np.sum(samp_en_roi)):
+            warn_with_log("There is NaN in the entropy values!")
+
+        return samp_en_roi.T  # 1 X n_roi
