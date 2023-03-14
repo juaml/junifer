ae06663a95b3464dc80e77bb916833ff81c21380
Fede Raimondo <f.raimondo@fz-juelich.de>
2023-03-14 23:06:25 +0100

    added test

--- junifer/api/tests/test_functions.py
+++ junifer/api/tests/test_functions.py
@@ -552,6 +552,58 @@ def test_queue_condor_conda_python(
             )
 
 
+def test_queue_condor_conda_pre_run_python(
+    tmp_path: Path,
+    monkeypatch: pytest.MonkeyPatch,
+    caplog: pytest.LogCaptureFixture,
+) -> None:
+    """Test conda Python environment check for HTCondor.
+
+    Parameters
+    ----------
+    tmp_path : pathlib.Path
+        The path to the test directory.
+    monkeypatch : pytest.MonkeyPatch
+        The monkeypatch object.
+    caplog : pytest.LogCaptureFixture
+        The logcapturefixture object.
+
+    """
+    with monkeypatch.context() as m:
+        m.chdir(tmp_path)
+        with caplog.at_level(logging.INFO):
+            queue(
+                config={"elements": "sub-001"},
+                kind="HTCondor",
+                jobname="conda_env_check",
+                env={"kind": "conda", "name": "conda-env"},
+                pre_run="echo testing",
+            )
+            assert "Copying" in caplog.text
+            assert (
+                Path(
+                    tmp_path
+                    / "junifer_jobs"
+                    / "conda_env_check"
+                    / "run_conda.sh"
+                )
+                .stat()
+                .st_mode
+                == 33261
+            )
+
+            assert (
+                Path(
+                    tmp_path
+                    / "junifer_jobs"
+                    / "conda_env_check"
+                    / "pre_run.sh"
+                )
+                .stat()
+                .st_mode
+                == 33261
+            )
+
 def test_queue_condor_venv_python(
     tmp_path: Path,
     monkeypatch: pytest.MonkeyPatch,
