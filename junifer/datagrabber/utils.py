615c475b7e2d93dbe0c793197c9308e1608024c5
Synchon Mandal <synchon@protonmail.com>
2023-07-13 13:47:07 +0200

    update: allow subset of types found in patterns to be valid during checks

--- junifer/datagrabber/utils.py
+++ junifer/datagrabber/utils.py
@@ -77,9 +77,9 @@ def validate_patterns(types: List[str], patterns: Dict[str, str]) -> None:
     if not isinstance(patterns, dict):
         raise_error(msg="`patterns` must be a dict.", klass=TypeError)
     # Unequal length of objects
-    if len(types) != len(patterns):
+    if len(types) > len(patterns):
         raise_error(
-            msg="`types` and `patterns` must have the same length.",
+            msg="Length of `types` more than that of `patterns`.",
             klass=ValueError,
         )
 
