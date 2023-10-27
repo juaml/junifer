c0a0eb1f5c20d05d1eb5d8395385988d0eca5e09
Synchon Mandal <synchon@protonmail.com>
2023-10-27 16:45:28 +0200

    chore: improve commentary in UpdateMetaMixin

--- junifer/pipeline/update_meta_mixin.py
+++ junifer/pipeline/update_meta_mixin.py
@@ -23,19 +23,25 @@ class UpdateMetaMixin:
             The data object to update.
         step_name : str
             The name of the pipeline step.
+
         """
+        # Initialize empty dictionary for the step's metadata
         t_meta = {}
+        # Set class name for the step
         t_meta["class"] = self.__class__.__name__
+        # Add object variables to metadata if name doesn't start with "_"
         for k, v in vars(self).items():
             if not k.startswith("_"):
                 t_meta[k] = v
-
+        # Add "meta" to the step's local context dict
         if "meta" not in input:
             input["meta"] = {}
+        # Add step name
         input["meta"][step_name] = t_meta
+        # Add step dependencies
         if "dependencies" not in input["meta"]:
             input["meta"]["dependencies"] = set()
-
+        # Update step dependencies
         dependencies = getattr(self, "_DEPENDENCIES", set())
         if dependencies is not None:
             if not isinstance(dependencies, (set, list)):
