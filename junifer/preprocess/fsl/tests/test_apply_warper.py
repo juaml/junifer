0dfa55bd5365196c68c5b0946b4eb62d2649a731
Synchon Mandal <synchon@protonmail.com>
2024-01-10 10:26:43 +0100

    update: complete tests for ApplyWarper

--- junifer/preprocess/fsl/tests/test_apply_warper.py
+++ junifer/preprocess/fsl/tests/test_apply_warper.py
@@ -3,12 +3,16 @@
 # Authors: Synchon Mandal <s.mandal@fz-juelich.de>
 # License: AGPL
 
+import socket
+from pathlib import Path
 from typing import List
 
+import nibabel as nib
 import pytest
 
-# from junifer.datareader import DefaultDataReader
-# from junifer.pipeline.utils import _check_fsl
+from junifer.datagrabber import DataladHCP1200
+from junifer.datareader import DefaultDataReader
+from junifer.pipeline.utils import _check_fsl
 from junifer.preprocess.fsl.apply_warper import _ApplyWarper
 
 
@@ -47,27 +51,54 @@ def test_ApplyWarper_get_output_type(input_: List[str]) -> None:
     assert apply_warper.get_output_type(input_) == input_
 
 
-@pytest.mark.skip(reason="requires testing dataset")
-# @pytest.mark.skipif(
-#     _check_fsl() is False, reason="requires fsl to be in PATH"
-# )
+@pytest.mark.skipif(_check_fsl() is False, reason="requires FSL to be in PATH")
+@pytest.mark.skipif(
+    socket.gethostname() != "juseless",
+    reason="only for juseless",
+)
 def test_ApplyWarper__run_applywarp() -> None:
     """Test ApplyWarper _run_applywarp."""
-    # Initialize datareader
-    # reader = DefaultDataReader()
-    # Initialize preprocessor
-    # bold_warper = _ApplyWarper(reference="T1w", on="BOLD")
-    # TODO(synchon): setup datagrabber and run pipeline
+    with DataladHCP1200(
+        tasks=["REST1"],
+        phase_encodings=["LR"],
+        ica_fix=True,
+    ) as dg:
+        # Read data
+        element_data = DefaultDataReader().fit_transform(
+            dg[("100206", "REST1", "LR")]
+        )
+        # Preprocess data
+        warped_data, resampled_ref_path = _ApplyWarper(
+            reference="T1w", on="BOLD"
+        )._run_applywarp(
+            input_data=element_data["BOLD"],
+            ref_path=element_data["T1w"]["path"],
+            warp_path=element_data["Warp"]["path"],
+        )
+        assert isinstance(warped_data, nib.Nifti1Image)
+        assert isinstance(resampled_ref_path, Path)
 
 
-@pytest.mark.skip(reason="requires testing dataset")
-# @pytest.mark.skipif(
-#     _check_fsl() is False, reason="requires fsl to be in PATH"
-# )
+@pytest.mark.skipif(_check_fsl() is False, reason="requires FSL to be in PATH")
+@pytest.mark.skipif(
+    socket.gethostname() != "juseless",
+    reason="only for juseless",
+)
 def test_ApplyWarper_preprocess() -> None:
     """Test ApplyWarper preprocess."""
-    # Initialize datareader
-    # reader = DefaultDataReader()
-    # Initialize preprocessor
-    # bold_warper = _ApplyWarper(reference="T1w", on="BOLD")
-    # TODO(synchon): setup datagrabber and run pipeline
+    with DataladHCP1200(
+        tasks=["REST1"],
+        phase_encodings=["LR"],
+        ica_fix=True,
+    ) as dg:
+        # Read data
+        element_data = DefaultDataReader().fit_transform(
+            dg[("100206", "REST1", "LR")]
+        )
+        # Preprocess data
+        data_type, data = _ApplyWarper(reference="T1w", on="BOLD").preprocess(
+            input=element_data["BOLD"],
+            extra_input=element_data,
+        )
+        assert isinstance(data_type, str)
+        assert isinstance(data, dict)
