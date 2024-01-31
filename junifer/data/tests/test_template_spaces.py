e645ff8daad4004692580c54023897457f4a7f02
Synchon Mandal <synchon@protonmail.com>
2024-01-31 09:52:56 +0100

    update: add tests for get_template

--- junifer/data/tests/test_template_spaces.py
+++ junifer/data/tests/test_template_spaces.py
@@ -6,9 +6,12 @@
 import socket
 from pathlib import Path
 
+import nibabel as nib
 import pytest
 
-from junifer.data import get_xfm
+from junifer.data import get_template, get_xfm
+from junifer.datareader import DefaultDataReader
+from junifer.testing.datagrabbers import OasisVBMTestingDataGrabber
 
 
 @pytest.mark.skipif(
@@ -28,3 +31,42 @@ def test_get_xfm(tmp_path: Path) -> None:
         src="MNI152NLin6Asym", dst="MNI152NLin2009cAsym", xfms_dir=tmp_path
     )
     assert isinstance(xfm_path, Path)
+
+
+def test_get_template() -> None:
+    """Test tailored template image fetch."""
+    with OasisVBMTestingDataGrabber() as dg:
+        element = dg["sub-01"]
+        element_data = DefaultDataReader().fit_transform(element)
+        vbm_gm = element_data["VBM_GM"]
+        # Get tailored parcellation
+        tailored_template = get_template(
+            space=vbm_gm["space"], target_data=vbm_gm
+        )
+        assert isinstance(tailored_template, nib.Nifti1Image)
+
+
+def test_get_template_invalid_space() -> None:
+    """Test invalid space check for template fetch."""
+    with OasisVBMTestingDataGrabber() as dg:
+        element = dg["sub-01"]
+        element_data = DefaultDataReader().fit_transform(element)
+        vbm_gm = element_data["VBM_GM"]
+        # Get tailored parcellation
+        with pytest.raises(ValueError, match="Unknown template space:"):
+            _ = get_template(space="andromeda", target_data=vbm_gm)
+
+
+def test_get_template_invalid_resolution() -> None:
+    """Test invalid resolution check for template fetch."""
+    with OasisVBMTestingDataGrabber() as dg:
+        element = dg["sub-01"]
+        element_data = DefaultDataReader().fit_transform(element)
+        vbm_gm = element_data["VBM_GM"]
+        # Change header resolution to raise error
+        element_data["VBM_GM"]["data"].header.set_zooms((3, 3, 3))
+        # Get tailored parcellation
+        with pytest.raises(
+            RuntimeError, match="not found in the required resolution"
+        ):
+            _ = get_template(space=vbm_gm["space"], target_data=vbm_gm)
