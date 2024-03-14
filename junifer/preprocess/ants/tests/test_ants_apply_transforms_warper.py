3911f946211df54e3705a72454dec78a925db7ab
Synchon Mandal <synchon@protonmail.com>
2024-03-14 11:43:50 +0100

    chore: remove unnecessary tests for _ApplyWarper and _AntsApplyTransformsWarper

--- junifer/preprocess/ants/tests/test_ants_apply_transforms_warper.py
+++ junifer/preprocess/ants/tests/test_ants_apply_transforms_warper.py
@@ -4,9 +4,7 @@
 # License: AGPL
 
 import socket
-from pathlib import Path
 
-import nibabel as nib
 import pytest
 
 from junifer.datagrabber import DMCC13Benchmark
@@ -24,39 +22,6 @@ def test_AntsApplyTransformsWarper_init() -> None:
     )
     assert ants_apply_transforms_warper.ref == "T1w"
     assert ants_apply_transforms_warper.on == "BOLD"
-
-
-@pytest.mark.skipif(
-    _check_ants() is False, reason="requires ANTs to be in PATH"
-)
-@pytest.mark.skipif(
-    socket.gethostname() != "juseless",
-    reason="only for juseless",
-)
-def test_AntsApplyTransformsWarper__run_apply_transform() -> None:
-    """Test AntsApplyTransformsWarper _run_apply_transform."""
-    with DMCC13Benchmark(
-        types=["BOLD", "T1w", "Warp"],
-        sessions=["wave1bas"],
-        tasks=["Rest"],
-        phase_encodings=["AP"],
-        runs=["1"],
-        native_t1w=True,
-    ) as dg:
-        # Read data
-        element_data = DefaultDataReader().fit_transform(
-            dg[("f9057kp", "wave1bas", "Rest", "AP", "1")]
-        )
-        # Preprocess data
-        warped_data, resampled_ref_path = _AntsApplyTransformsWarper(
-            reference="T1w", on="BOLD"
-        )._run_apply_transforms(
-            input_data=element_data["BOLD"],
-            ref_path=element_data["T1w"]["path"],
-            warp_path=element_data["Warp"]["path"],
-        )
-        assert isinstance(warped_data, nib.Nifti1Image)
-        assert isinstance(resampled_ref_path, Path)
 
 
 @pytest.mark.skipif(
--- junifer/preprocess/fsl/tests/test_apply_warper.py
+++ junifer/preprocess/fsl/tests/test_apply_warper.py
@@ -4,9 +4,7 @@
 # License: AGPL
 
 import socket
-from pathlib import Path
 
-import nibabel as nib
 import pytest
 
 from junifer.datagrabber import DataladHCP1200
@@ -20,34 +18,6 @@ def test_ApplyWarper_init() -> None:
     apply_warper = _ApplyWarper(reference="T1w", on="BOLD")
     assert apply_warper.ref == "T1w"
     assert apply_warper.on == "BOLD"
-
-
-@pytest.mark.skipif(_check_fsl() is False, reason="requires FSL to be in PATH")
-@pytest.mark.skipif(
-    socket.gethostname() != "juseless",
-    reason="only for juseless",
-)
-def test_ApplyWarper__run_applywarp() -> None:
-    """Test ApplyWarper _run_applywarp."""
-    with DataladHCP1200(
-        tasks=["REST1"],
-        phase_encodings=["LR"],
-        ica_fix=True,
-    ) as dg:
-        # Read data
-        element_data = DefaultDataReader().fit_transform(
-            dg[("100206", "REST1", "LR")]
-        )
-        # Preprocess data
-        warped_data, resampled_ref_path = _ApplyWarper(
-            reference="T1w", on="BOLD"
-        )._run_applywarp(
-            input_data=element_data["BOLD"],
-            ref_path=element_data["T1w"]["path"],
-            warp_path=element_data["Warp"]["path"],
-        )
-        assert isinstance(warped_data, nib.Nifti1Image)
-        assert isinstance(resampled_ref_path, Path)
 
 
 @pytest.mark.skipif(_check_fsl() is False, reason="requires FSL to be in PATH")
