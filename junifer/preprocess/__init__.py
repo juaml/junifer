6e705a344e19a78e3b4e2737265d02865af4db45
Synchon Mandal <synchon@protonmail.com>
2024-03-14 14:37:57 +0100

    chore: remove old NilearnSmoothing class and its imports

--- junifer/preprocess/__init__.py
+++ junifer/preprocess/__init__.py
@@ -9,4 +9,4 @@ from .base import BasePreprocessor
 from .confounds import fMRIPrepConfoundRemover
 from .bold_warper import BOLDWarper
 from .warping import SpaceWarper
-from .smoothing import NilearnSmoothing
+from .smoothing import Smoothing
--- junifer/preprocess/smoothing/__init__.py
+++ junifer/preprocess/smoothing/__init__.py
@@ -3,4 +3,4 @@
 # Authors: Synchon Mandal <s.mandal@fz-juelich.de>
 # License: AGPL
 
-from .nilearn_smoothing import NilearnSmoothing
+from .smoothing import Smoothing
