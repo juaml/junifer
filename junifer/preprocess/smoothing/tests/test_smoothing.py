afa613a423784f22ed0f166a1050818e4f91e05b
Synchon Mandal <synchon@protonmail.com>
2024-03-18 13:26:16 +0100

    feature: introduce FSLSmoothing

--- junifer/preprocess/smoothing/tests/test_smoothing.py
+++ junifer/preprocess/smoothing/tests/test_smoothing.py
@@ -7,7 +7,7 @@
 import pytest
 
 from junifer.datareader import DefaultDataReader
-from junifer.pipeline.utils import _check_afni
+from junifer.pipeline.utils import _check_afni, _check_fsl
 from junifer.preprocess import Smoothing
 from junifer.testing.datagrabbers import SPMAuditoryTestingDataGrabber
 
@@ -62,6 +62,33 @@ def test_Smoothing_afni(data_type: str) -> None:
             using="afni",
             on=data_type,
             smoothing_params={"fwhm": 3},
+        ).fit_transform(element_data)
+
+        assert isinstance(output, dict)
+
+
+@pytest.mark.parametrize(
+    "data_type",
+    ["T1w", "BOLD"],
+)
+@pytest.mark.skipif(_check_fsl() is False, reason="requires FSL to be in PATH")
+def test_Smoothing_fsl(data_type: str) -> None:
+    """Test Smoothing using FSL.
+
+    Parameters
+    ----------
+    data_type : str
+        The parametrized data type.
+
+    """
+    with SPMAuditoryTestingDataGrabber() as dg:
+        # Read data
+        element_data = DefaultDataReader().fit_transform(dg["sub001"])
+        # Preprocess data
+        output = Smoothing(
+            using="fsl",
+            on=data_type,
+            smoothing_params={"brightness_threshold": 10.0, "fwhm": 3.0},
         ).fit_transform(element_data)
 
         assert isinstance(output, dict)
