37eff009646bfbeb41e142338e71e20e35cb29bc
Synchon Mandal <synchon@protonmail.com>
2024-04-03 18:13:10 +0200

    update: improve docstring and general clean up for PatternDataladDataGrabber

--- junifer/datagrabber/pattern_datalad.py
+++ junifer/datagrabber/pattern_datalad.py
@@ -5,12 +5,11 @@
 #          Synchon Mandal <s.mandal@fz-juelich.de>
 # License: AGPL
 
-from typing import Dict, List
 
 from ..api.decorators import register_datagrabber
+from ..utils import logger
 from .datalad_base import DataladDataGrabber
 from .pattern import PatternDataGrabber
-from .utils import validate_patterns
 
 
 @register_datagrabber
@@ -25,11 +24,109 @@ class PatternDataladDataGrabber(DataladDataGrabber, PatternDataGrabber):
     types : list of str
         The types of data to be grabbed.
     patterns : dict
-        Patterns for each type of data as a dictionary. The keys are the types
-        and the values are the patterns. Each occurrence of the string
-        ``{subject}`` in the pattern will be replaced by the indexed element.
-    **kwargs
-        Keyword arguments passed to superclass.
+        Data type patterns as a dictionary. It has the following schema:
+
+        * ``"T1w"`` :
+
+          .. code-block:: none
+
+            {
+              "mandatory": ["pattern", "space"],
+              "optional": []
+            }
+
+        * ``"T2w"`` :
+
+          .. code-block:: none
+
+            {
+              "mandatory": ["pattern", "space"],
+              "optional": []
+            }
+
+        * ``"BOLD"`` :
+
+          .. code-block:: none
+
+            {
+              "mandatory": ["pattern", "space"],
+              "optional": ["mask_item"]
+            }
+
+        * ``"Warp"`` :
+
+          .. code-block:: none
+
+            {
+              "mandatory": ["pattern", "src", "dst"],
+              "optional": []
+            }
+
+        * ``"BOLD_confounds"`` :
+
+          .. code-block:: none
+
+            {
+              "mandatory": ["pattern", "format"],
+              "optional": []
+            }
+
+        * ``"VBM_GM"`` :
+
+          .. code-block:: none
+
+            {
+              "mandatory": ["pattern", "space"],
+              "optional": []
+            }
+
+        * ``"VBM_WM"`` :
+
+          .. code-block:: none
+
+            {
+              "mandatory": ["pattern", "space"],
+              "optional": []
+            }
+
+        Basically, for each data type, one needs to provide ``mandatory`` keys
+        and can choose to also provide ``optional`` keys. The value for each
+        key is a string. So, one needs to provide necessary data types as a
+        dictionary, for example:
+
+        .. code-block:: none
+
+          {
+              "BOLD": {
+                "pattern": "...",
+                "space": "...",
+              },
+              "T1w": {
+                "pattern": "...",
+                "space": "...",
+              },
+              "Warp": {
+                "pattern": "...",
+                "src": "...",
+                "dst": "...",
+              }
+          }
+
+        taken from :class:`.HCP1200`.
+    replacements : str or list of str
+        Replacements in the ``pattern`` key of each data type. The value needs
+        to be a list of all possible replacements.
+    confounds_format : {"fmriprep", "adhoc"} or None, optional
+        The format of the confounds for the dataset (default None).
+    datadir : str or pathlib.Path or None, optional
+        That directory where the datalad dataset will be cloned. If None,
+        the datalad dataset will be cloned into a temporary directory
+        (default None).
+    rootdir : str or pathlib.Path, optional
+        The path within the datalad dataset to the root directory
+        (default ".").
+    uri : str or None, optional
+        URI of the datalad sibling (default None).
 
     See Also
     --------
@@ -42,12 +139,13 @@ class PatternDataladDataGrabber(DataladDataGrabber, PatternDataGrabber):
 
     def __init__(
         self,
-        types: List[str],
-        patterns: Dict[str, str],
         **kwargs,
     ) -> None:
-        # Validate patterns
-        validate_patterns(types=types, patterns=patterns)
+        # TODO(synchon): needs to be reworked, DataladDataGrabber needs to be
+        # a mixin to avoid multiple inheritance wherever possible.
 
-        super().__init__(types=types, patterns=patterns, **kwargs)
-        self.patterns = patterns
+        logger.debug("Initializing PatternDataladDataGrabber")
+        for key, val in kwargs.items():
+            logger.debug(f"\t{key} = {val}")
+
+        super().__init__(**kwargs)
