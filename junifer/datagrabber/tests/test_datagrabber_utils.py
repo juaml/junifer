744cc8d1c9e6b8956aa27cfba7a07de28af598e7
Synchon Mandal <synchon@protonmail.com>
2024-04-03 18:10:49 +0200

    chore: update tests for datagrabber.utils

--- junifer/datagrabber/tests/test_datagrabber_utils.py
+++ junifer/datagrabber/tests/test_datagrabber_utils.py
@@ -3,6 +3,9 @@
 # Authors: Federico Raimondo <f.raimondo@fz-juelich.de>
 # License: AGPL
 
+from contextlib import nullcontext
+from typing import ContextManager, Dict, List, Union
+
 import pytest
 
 from junifer.datagrabber.utils import (
@@ -12,79 +15,204 @@ from junifer.datagrabber.utils import (
 )
 
 
-def test_validate_types() -> None:
-    """Test validation of types."""
-    with pytest.raises(TypeError, match="must be a list"):
-        validate_types("wrong")  # type: ignore
-    with pytest.raises(TypeError, match="must be a list of strings"):
-        validate_types([1])  # type: ignore
+@pytest.mark.parametrize(
+    "types, expect",
+    [
+        ("wrong", pytest.raises(TypeError, match="must be a list")),
+        ([1], pytest.raises(TypeError, match="must be a list of strings")),
+        (["T1w", "BOLD"], nullcontext()),
+    ],
+)
+def test_validate_types(
+    types: Union[str, List[str], List[int]],
+    expect: ContextManager,
+) -> None:
+    """Test validation of types.
 
-    validate_types(["T1w", "BOLD"])
+    Parameters
+    ----------
+    types : str, list of int or str
+        The parametrized data types to validate.
+    expect : typing.ContextManager
+        The parametrized ContextManager object.
 
-
-def test_validate_replacements() -> None:
-    """Test validation of replacements."""
-    with pytest.raises(TypeError, match="must be a list"):
-        validate_replacements("wrong", "also wrong")  # type: ignore
-    with pytest.raises(TypeError, match="must be a dict"):
-        validate_replacements(["correct"], "wrong")  # type: ignore
-
-    patterns = {
-        "T1w": "{subject}/anat/{subject}_T1w.nii.gz",
-        "BOLD": "{subject}/func/{subject}_task-rest_bold.nii.gz",
-    }
-
-    with pytest.raises(TypeError, match="must be a list of strings"):
-        validate_replacements([1], patterns)  # type: ignore
-
-    with pytest.raises(ValueError, match="is not part of"):
-        validate_replacements(["session"], patterns)
-
-    wrong_patterns = {
-        "T1w": "{subject}/anat/_T1w.nii.gz",
-        "BOLD": "{session}/func/_task-rest_bold.nii.gz",
-    }
-
-    with pytest.raises(ValueError, match="At least one pattern"):
-        validate_replacements(["subject", "session"], wrong_patterns)
-
-    validate_replacements(["subject"], patterns)
+    """
+    with expect:
+        validate_types(types)  # type: ignore
 
 
-def test_validate_patterns() -> None:
-    """Test validation of patterns."""
-    types = ["T1w", "BOLD"]
-    with pytest.raises(TypeError, match="must be a dict"):
-        validate_patterns(types, "wrong")  # type: ignore
+@pytest.mark.parametrize(
+    "replacements, patterns, expect",
+    [
+        (
+            "wrong",
+            "also wrong",
+            pytest.raises(TypeError, match="must be a list"),
+        ),
+        (
+            [1],
+            {
+                "T1w": {"pattern": "{subject}/anat/{subject}_T1w.nii.gz"},
+                "BOLD": {
+                    "pattern": "{subject}/func/{subject}_task-rest_bold.nii.gz"
+                },
+            },
+            pytest.raises(TypeError, match="must be a list of strings"),
+        ),
+        (
+            ["session"],
+            {
+                "T1w": {"pattern": "{subject}/anat/{subject}_T1w.nii.gz"},
+                "BOLD": {
+                    "pattern": "{subject}/func/{subject}_task-rest_bold.nii.gz"
+                },
+            },
+            pytest.raises(ValueError, match="is not part of"),
+        ),
+        (
+            ["subject", "session"],
+            {
+                "T1w": {"pattern": "{subject}/anat/_T1w.nii.gz"},
+                "BOLD": {"pattern": "{session}/func/_task-rest_bold.nii.gz"},
+            },
+            pytest.raises(ValueError, match="At least one pattern"),
+        ),
+        (
+            ["subject"],
+            {
+                "T1w": {"pattern": "{subject}/anat/{subject}_T1w.nii.gz"},
+                "BOLD": {
+                    "pattern": "{subject}/func/{subject}_task-rest_bold.nii.gz"
+                },
+            },
+            nullcontext(),
+        ),
+    ],
+)
+def test_validate_replacements(
+    replacements: Union[str, List[str], List[int]],
+    patterns: Union[str, Dict[str, Dict[str, str]]],
+    expect: ContextManager,
+) -> None:
+    """Test validation of replacements.
 
-    wrongpatterns = {
-        "T1w": "{subject}/anat/{subject}_T1w.nii.gz",
-    }
+    Parameters
+    ----------
+    replacements : str, list of str or int
+        The parametrized pattern replacements to validate.
+    patterns : str, dict
+        The parametrized patterns to validate against.
+    expect : typing.ContextManager
+        The parametrized ContextManager object.
 
-    with pytest.raises(
-        ValueError, match="Length of `types` more than that of `patterns`."
-    ):
-        validate_patterns(types, wrongpatterns)  # type: ignore
+    """
+    with expect:
+        validate_replacements(replacements=replacements, patterns=patterns)  # type: ignore
 
-    wrongpatterns = {
-        "T1w": "{subject}/anat/{subject}_T1w.nii.gz",
-        "T2": "{subject}/anat/{subject}_T2.nii.gz",
-    }
 
-    with pytest.raises(ValueError, match="contain all"):
-        validate_patterns(types, wrongpatterns)  # type: ignore
+@pytest.mark.parametrize(
+    "types, patterns, expect",
+    [
+        (
+            ["T1w", "BOLD"],
+            "wrong",
+            pytest.raises(TypeError, match="must be a dict"),
+        ),
+        (
+            ["T1w", "BOLD"],
+            {
+                "T1w": {"pattern": "{subject}/anat/{subject}_T1w.nii.gz"},
+            },
+            pytest.raises(
+                ValueError,
+                match="Length of `types` more than that of `patterns`.",
+            ),
+        ),
+        (
+            ["T1w", "BOLD"],
+            {
+                "T1w": {"pattern": "{subject}/anat/{subject}_T1w.nii.gz"},
+                "T2w": {"pattern": "{subject}/anat/{subject}_T2w.nii.gz"},
+            },
+            pytest.raises(ValueError, match="contain all"),
+        ),
+        (
+            ["T3w"],
+            {
+                "T3w": {"pattern": "{subject}/anat/{subject}_T3w.nii.gz"},
+            },
+            pytest.raises(ValueError, match="Unknown data type"),
+        ),
+        (
+            ["BOLD"],
+            {
+                "BOLD": {"patterns": "{subject}/func/{subject}_BOLD.nii.gz"},
+            },
+            pytest.raises(KeyError, match="Mandatory key"),
+        ),
+        (
+            ["BOLD_confounds"],
+            {
+                "BOLD_confounds": {
+                    "pattern": "{subject}/func/{subject}_confounds.tsv",
+                    "format": "fmriprep",
+                    "space": "MNINLin6Asym",
+                },
+            },
+            pytest.raises(RuntimeError, match="not accepted"),
+        ),
+        (
+            ["T1w"],
+            {
+                "T1w": {
+                    "pattern": "{subject}/anat/{subject}*.nii",
+                    "space": "native",
+                },
+            },
+            pytest.raises(ValueError, match="following a replacement"),
+        ),
+        (
+            ["T1w", "T2w", "BOLD", "BOLD_confounds"],
+            {
+                "T1w": {
+                    "pattern": "{subject}/anat/{subject}_T1w.nii.gz",
+                    "space": "native",
+                },
+                "T2w": {
+                    "pattern": "{subject}/anat/{subject}_T2w.nii.gz",
+                    "space": "native",
+                },
+                "BOLD": {
+                    "pattern": (
+                        "{subject}/func/{subject}_task-rest_bold.nii.gz"
+                    ),
+                    "space": "MNI152NLin6Asym",
+                },
+                "BOLD_confounds": {
+                    "pattern": "{subject}/func/{subject}_confounds.tsv",
+                    "format": "fmriprep",
+                },
+            },
+            nullcontext(),
+        ),
+    ],
+)
+def test_validate_patterns(
+    types: List[str],
+    patterns: Union[str, Dict[str, Dict[str, str]]],
+    expect: ContextManager,
+) -> None:
+    """Test validation of patterns.
 
-    patterns = {
-        "T1w": "{subject}/anat/{subject}_T1w.nii.gz",
-        "BOLD": "{subject}/func/{subject}_task-rest_bold.nii.gz",
-    }
+    Parameters
+    ----------
+    types : list of str
+        The parametrized data types.
+    patterns : str, dict
+        The patterns to validate.
+    expect : typing.ContextManager
+        The parametrized ContextManager object.
 
-    wrongpatterns = {
-        "T1w": "{subject}/anat/{subject}*.nii",
-        "BOLD": "{subject}/func/{subject}_task-rest_bold.nii.gz",
-    }
-
-    with pytest.raises(ValueError, match="following a replacement"):
-        validate_patterns(types, wrongpatterns)
-
-    validate_patterns(types, patterns)
+    """
+    with expect:
+        validate_patterns(types=types, patterns=patterns)  # type: ignore
