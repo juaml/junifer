2a9229ff215e793666c51f2aa2a8369f607cff2b
Synchon Mandal <synchon@protonmail.com>
2024-07-03 12:08:05 +0200

    chore: lint

--- junifer/pipeline/tests/test_registry.py
+++ junifer/pipeline/tests/test_registry.py
@@ -101,7 +101,7 @@ def test_get_class():
     register(step="datagrabber", name="bar", klass=str)
     # Get class
     obj = get_class(step="datagrabber", name="bar")
-    assert obj == str
+    assert isinstance(obj, str)
 
 
 # TODO: possible parametrization?
