f5e28ecb12625ae0db206517a77d770e04ede7bf
Synchon Mandal <synchon@protonmail.com>
2024-07-16 12:45:56 +0200

    feat: add deep_update() helper for dict update in varying width and depth

--- junifer/utils/__init__.py
+++ junifer/utils/__init__.py
@@ -6,7 +6,7 @@
 
 from .fs import make_executable
 from .logging import configure_logging, logger, raise_error, warn_with_log
-from .helpers import run_ext_cmd
+from .helpers import run_ext_cmd, deep_update
 
 
 __all__ = [
@@ -16,4 +16,5 @@ __all__ = [
     "raise_error",
     "warn_with_log",
     "run_ext_cmd",
+    "deep_update",
 ]
