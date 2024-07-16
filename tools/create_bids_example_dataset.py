dc5ca201eb438919379a16acb3a7e3e8ddfeeed1
Synchon Mandal <synchon@protonmail.com>
2024-07-16 12:49:59 +0200

    chore: update scripts for example bids datasets in gin

--- tools/create_bids_example_dataset.py
+++ tools/create_bids_example_dataset.py
@@ -1,34 +1,40 @@
 # Authors: Federico Raimondo <f.raimondo@fz-juelich.de>
 # License: AGPL
-from tempfile import TemporaryDirectory
 from pathlib import Path
+from tempfile import TemporaryDirectory
 
 import datalad.api as dl
 
-dst = 'git@gin.g-node.org:/juaml/datalad-example-bids.git'
+
+dst = "git@gin.g-node.org:/juaml/datalad-example-bids.git"
 
 with TemporaryDirectory() as tmpdir_name:
     tmpdir = Path(tmpdir_name)
     ds = dl.create(tmpdir)  # type: ignore
 
-    base_dir = tmpdir / 'example_bids'
+    base_dir = tmpdir / "example_bids"
     base_dir.mkdir()
 
     for i_sub in range(1, 10):
-        t_sub = f'sub-{i_sub:02d}'
+        t_sub = f"sub-{i_sub:02d}"
         sub_dir = base_dir / t_sub
         sub_dir.mkdir()
 
-        for dname in ['anat', 'func']:
+        for dname in ["anat", "func"]:
             (sub_dir / dname).mkdir()
 
-        fnames = [f'anat/{t_sub}_T1w.nii.gz',
-                  f'func/{t_sub}_task-rest_bold.nii.gz',
-                  f'func/{t_sub}_task-rest_bold.json']
+        fnames = [
+            f"anat/{t_sub}_T1w.nii.gz",
+            f"anat/{t_sub}_brain_mask.nii.gz",
+            f"func/{t_sub}_task-rest_bold.nii.gz",
+            f"func/{t_sub}_task-rest_bold.json",
+            f"func/{t_sub}_task-rest_brain_mask.nii.gz",
+            f"func/{t_sub}_task-rest_confounds_regressors.tsv",
+        ]
         for fname in fnames:
-            with open(sub_dir / fname, 'w') as f:
-                f.write(f'placeholder-{fname}')
+            with open(sub_dir / fname, "w") as f:
+                f.write(f"placeholder-{fname}")
 
     ds.save(recursive=True)
-    ds.siblings('add', name='gin', url=dst)
-    ds.push(to='gin', force='all')
+    ds.siblings("add", name="gin", url=dst)
+    ds.push(to="gin", force="all")
--- tools/create_bids_example_dataset_sessions.py
+++ tools/create_bids_example_dataset_sessions.py
@@ -1,41 +1,50 @@
 # Authors: Federico Raimondo <f.raimondo@fz-juelich.de>
 # License: AGPL
-from tempfile import TemporaryDirectory
 from pathlib import Path
+from tempfile import TemporaryDirectory
 
 import datalad.api as dl
 
-dst = 'git@gin.g-node.org:/juaml/datalad-example-bids-ses.git'
+
+dst = "git@gin.g-node.org:/juaml/datalad-example-bids-ses.git"
 
 with TemporaryDirectory() as tmpdir_name:
     tmpdir = Path(tmpdir_name)
     ds = dl.create(tmpdir)  # type: ignore
 
-    base_dir = tmpdir / 'example_bids_ses'
+    base_dir = tmpdir / "example_bids_ses"
     base_dir.mkdir()
 
     for i_sub in range(1, 10):
-        t_sub = f'sub-{i_sub:02d}'
+        t_sub = f"sub-{i_sub:02d}"
         sub_dir = base_dir / t_sub
         sub_dir.mkdir()
 
         for i_ses in range(1, 4):
-            t_ses = f'ses-{i_ses:02d}'
+            t_ses = f"ses-{i_ses:02d}"
             ses_dir = sub_dir / t_ses
             ses_dir.mkdir()
 
-            for dname in ['anat', 'func']:
+            for dname in ["anat", "func"]:
                 (ses_dir / dname).mkdir()
 
-            fnames = [f'anat/{t_sub}_{t_ses}_T1w.nii.gz']
+            fnames = [
+                f"anat/{t_sub}_{t_ses}_T1w.nii.gz",
+                f"anat/{t_sub}_{t_ses}_brain_mask.nii.gz",
+            ]
             if i_ses != 3:  # Session 3 does not have functional data
-                fnames.extend([
-                    f'func/{t_sub}_{t_ses}_task-rest_bold.nii.gz',
-                    f'func/{t_sub}_{t_ses}_task-rest_bold.json'])
+                fnames.extend(
+                    [
+                        f"func/{t_sub}_{t_ses}_task-rest_bold.nii.gz",
+                        f"func/{t_sub}_{t_ses}_task-rest_bold.json",
+                        f"func/{t_sub}_{t_ses}_task-rest_brain_mask.nii.gz",
+                        f"func/{t_sub}_{t_ses}_task-rest_confounds_regressors.tsv",
+                    ]
+                )
             for fname in fnames:
-                with open(ses_dir / fname, 'w') as f:
-                    f.write('placeholder-{fname}')
+                with open(ses_dir / fname, "w") as f:
+                    f.write("placeholder-{fname}")
 
     ds.save(recursive=True)
-    ds.siblings('add', name='gin', url=dst)
-    ds.push(to='gin', force='all')
+    ds.siblings("add", name="gin", url=dst)
+    ds.push(to="gin", force="all")
