ac23f927961e9acae09a413bfa85964619fa8805
Synchon Mandal <synchon@protonmail.com>
2024-07-25 14:46:33 +0200

    update: lazy import for junifer

---
+++ junifer/__init__.pyi
@@ -0,0 +1,33 @@
+__all__ = [
+    "api",
+    "configs",
+    "data",
+    "datagrabber",
+    "datareader",
+    "markers",
+    "pipeline",
+    "preprocess",
+    "stats",
+    "storage",
+    "utils",
+    "external",
+    "onthefly",
+    "__version__",
+]
+
+from . import (
+    api,
+    configs,
+    data,
+    datagrabber,
+    datareader,
+    markers,
+    pipeline,
+    preprocess,
+    stats,
+    storage,
+    utils,
+    external,
+    onthefly,
+)
+from ._version import __version__
