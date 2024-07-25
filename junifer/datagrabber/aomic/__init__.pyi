4438d8048c00907712705dda8527e8d85e14639b
Synchon Mandal <synchon@protonmail.com>
2024-07-25 14:32:16 +0200

    update: lazy import for datagrabber.aomic

---
+++ junifer/datagrabber/aomic/__init__.pyi
@@ -0,0 +1,5 @@
+__all__ = ["DataladAOMICID1000", "DataladAOMICPIOP1", "DataladAOMICPIOP2"]
+
+from .id1000 import DataladAOMICID1000
+from .piop1 import DataladAOMICPIOP1
+from .piop2 import DataladAOMICPIOP2
