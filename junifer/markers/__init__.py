f0da4072b7f2bc1fd21fc2aa27806a680566dc29
Synchon Mandal <synchon@protonmail.com>
2024-07-25 14:41:06 +0200

    update: lazy import for markers

--- junifer/markers/__init__.py
+++ junifer/markers/__init__.py
@@ -5,43 +5,7 @@
 #          Synchon Mandal <s.mandal@fz-juelich.de>
 # License: AGPL
 
-from .base import BaseMarker
-from .collection import MarkerCollection
-from .ets_rss import RSSETSMarker
-from .parcel_aggregation import ParcelAggregation
-from .sphere_aggregation import SphereAggregation
-from .functional_connectivity import (
-    FunctionalConnectivityParcels,
-    FunctionalConnectivitySpheres,
-    CrossParcellationFC,
-    EdgeCentricFCParcels,
-    EdgeCentricFCSpheres,
-)
-from .reho import ReHoParcels, ReHoSpheres
-from .falff import ALFFParcels, ALFFSpheres
-from .temporal_snr import (
-    TemporalSNRParcels,
-    TemporalSNRSpheres,
-)
-from .brainprint import BrainPrint
+import lazy_loader as lazy
 
 
-__all__ = [
-    "BaseMarker",
-    "MarkerCollection",
-    "RSSETSMarker",
-    "ParcelAggregation",
-    "SphereAggregation",
-    "FunctionalConnectivityParcels",
-    "FunctionalConnectivitySpheres",
-    "CrossParcellationFC",
-    "EdgeCentricFCParcels",
-    "EdgeCentricFCSpheres",
-    "ReHoParcels",
-    "ReHoSpheres",
-    "ALFFParcels",
-    "ALFFSpheres",
-    "TemporalSNRParcels",
-    "TemporalSNRSpheres",
-    "BrainPrint",
-]
+__getattr__, __dir__, __all__ = lazy.attach_stub(__name__, __file__)
