888e456687c26f70baef50e83c3a58c89d90177f
Synchon Mandal <synchon@protonmail.com>
2024-10-22 17:35:47 +0200

    update: improve tests for testing

--- junifer/testing/tests/test_testing_registry.py
+++ junifer/testing/tests/test_testing_registry.py
@@ -2,23 +2,28 @@
 
 import importlib
 
-from junifer.pipeline.registry import get_step_names
+from junifer.pipeline import PipelineComponentRegistry
 
 
 def test_testing_registry() -> None:
     """Test testing registry."""
     import junifer
 
-    importlib.reload(junifer.pipeline.registry)
+    importlib.reload(junifer.pipeline.pipeline_component_registry)
     importlib.reload(junifer)
 
-    assert "OasisVBMTestingDataGrabber" not in get_step_names("datagrabber")
-    assert "SPMAuditoryTestingDataGrabber" not in get_step_names("datagrabber")
-    assert "PartlyCloudyTestingDataGrabber" not in get_step_names(
+    step_components = PipelineComponentRegistry().step_components(
         "datagrabber"
     )
-    importlib.reload(junifer.testing.registry)  # type: ignore
+    assert "OasisVBMTestingDataGrabber" not in step_components
+    assert "SPMAuditoryTestingDataGrabber" not in step_components
+    assert "PartlyCloudyTestingDataGrabber" not in step_components
 
-    assert "OasisVBMTestingDataGrabber" in get_step_names("datagrabber")
-    assert "SPMAuditoryTestingDataGrabber" in get_step_names("datagrabber")
-    assert "PartlyCloudyTestingDataGrabber" in get_step_names("datagrabber")
+    importlib.import_module(junifer.testing.registry)
+
+    updated_step_components = PipelineComponentRegistry().step_components(
+        "datagrabber"
+    )
+    assert "OasisVBMTestingDataGrabber" in updated_step_components
+    assert "SPMAuditoryTestingDataGrabber" in updated_step_components
+    assert "PartlyCloudyTestingDataGrabber" in updated_step_components
