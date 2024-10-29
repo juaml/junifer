52ff44086a56f36f0bd1e737942acdf4e0632070
Synchon Mandal <synchon@protonmail.com>
2024-10-29 10:08:13 +0100

    feat!: introduce new *_data functions for easily interfacing with data registries

--- junifer/data/__init__.pyi
+++ junifer/data/__init__.pyi
@@ -2,6 +2,11 @@ __all__ = [
     "CoordinatesRegistry",
     "ParcellationRegistry",
     "MaskRegistry",
+    "get_data",
+    "list_data",
+    "load_data",
+    "register_data",
+    "deregister_data",
     "get_template",
     "get_xfm",
     "utils",
@@ -10,6 +15,14 @@ __all__ = [
 from .coordinates import CoordinatesRegistry
 from .parcellations import ParcellationRegistry
 from .masks import MaskRegistry
+
+from ._dispatch import (
+    get_data,
+    list_data,
+    load_data,
+    register_data,
+    deregister_data,
+)
 
 from .template_spaces import get_template, get_xfm
 
