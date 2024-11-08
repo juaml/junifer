3435256da826853e0253d01c67bf4406ef4ed134
Fede Raimondo <f.raimondo@fz-juelich.de>
2024-11-08 16:30:51 +0100

    Force delete signgletons

--- junifer/conftest.py
+++ junifer/conftest.py
@@ -12,8 +12,14 @@ from junifer.utils.singleton import Singleton
 def reset_singletons() -> None:
     """Reset all singletons."""
     to_clean = ["WorkDirManager"]
+    to_remove = [
+        v for k, v in Singleton.instances.items() if k.__name__ in to_clean
+    ]
     Singleton.instances = {
         k: v
         for k, v in Singleton.instances.items()
         if k.__name__ not in to_clean
     }
+    # Force deleting the singletons
+    for elem in to_remove:
+        del elem
