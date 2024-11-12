8d1354cd39d96a32c753a22a8f3aef5989147d8b
Synchon Mandal <synchon@protonmail.com>
2024-11-12 15:30:55 +0100

    feat: introduce typing module and type hints

---
+++ junifer/typing/__init__.py
@@ -0,0 +1,9 @@
+"""Type hints for internal and external use."""
+
+# Authors: Synchon Mandal <s.mandal@fz-juelich.de>
+# License: AGPL
+
+import lazy_loader as lazy
+
+
+__getattr__, __dir__, __all__ = lazy.attach_stub(__name__, __file__)
