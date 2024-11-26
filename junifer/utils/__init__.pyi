6396868803797a1eb44621be940a6f992cb5037d
Synchon Mandal <synchon@protonmail.com>
2024-11-26 11:06:44 +0100

    chore: import ConfigManager to docs ref

--- junifer/utils/__init__.pyi
+++ junifer/utils/__init__.pyi
@@ -8,10 +8,11 @@ __all__ = [
     "run_ext_cmd",
     "deep_update",
     "yaml",
+    "ConfigManager",
 ]
 
 from .fs import make_executable
 from .logging import configure_logging, logger, raise_error, warn_with_log
-from .config import config
+from .config import config, ConfigManager
 from .helpers import run_ext_cmd, deep_update
 from ._yaml import yaml
