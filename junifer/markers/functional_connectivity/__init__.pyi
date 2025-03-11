bcadae2ebe959a1a4e7a5a603d85c515256b438b
Synchon Mandal <synchon@protonmail.com>
2025-03-11 11:27:10 +0100

    feat: add FunctionalConnectivity{Parcels,Spheres} markers

--- junifer/markers/functional_connectivity/__init__.pyi
+++ junifer/markers/functional_connectivity/__init__.pyi
@@ -1,6 +1,8 @@
 __all__ = [
     "FunctionalConnectivityParcels",
     "FunctionalConnectivitySpheres",
+    "FunctionalConnectivityLaggedParcels",
+    "FunctionalConnectivityLaggedSpheres",
     "CrossParcellationFC",
     "EdgeCentricFCParcels",
     "EdgeCentricFCSpheres",
@@ -8,6 +10,12 @@ __all__ = [
 
 from .functional_connectivity_parcels import FunctionalConnectivityParcels
 from .functional_connectivity_spheres import FunctionalConnectivitySpheres
+from .functional_connectivity_lagged_parcels import (
+    FunctionalConnectivityLaggedParcels,
+)
+from .functional_connectivity_lagged_spheres import (
+    FunctionalConnectivityLaggedSpheres,
+)
 from .crossparcellation_functional_connectivity import CrossParcellationFC
 from .edge_functional_connectivity_parcels import EdgeCentricFCParcels
 from .edge_functional_connectivity_spheres import EdgeCentricFCSpheres
