554efba8a8a5f1f7e8585b53f7d3c93e8139f029
Synchon Mandal <synchon@protonmail.com>
2025-07-29 18:57:01 +0200

    feat: introduce DataObjectDumper, BaseDataDumpAsset, AssetDumperDispatcher and AssetLoaderDispatcher

--- junifer/pipeline/__init__.pyi
+++ junifer/pipeline/__init__.pyi
@@ -1,4 +1,8 @@
 __all__ = [
+    "AssetDumperDispatcher",
+    "AssetLoaderDispatcher",
+    "BaseDataDumpAsset",
+    "DataObjectDumper",
     "PipelineComponentRegistry",
     "PipelineStepMixin",
     "UpdateMetaMixin",
@@ -6,6 +10,12 @@ __all__ = [
     "MarkerCollection",
 ]
 
+from ._data_object_dumper import (
+    AssetDumperDispatcher,
+    AssetLoaderDispatcher,
+    BaseDataDumpAsset,
+    DataObjectDumper,
+)
 from .pipeline_component_registry import PipelineComponentRegistry
 from .pipeline_step_mixin import PipelineStepMixin
 from .update_meta_mixin import UpdateMetaMixin
