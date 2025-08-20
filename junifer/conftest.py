44d3ffb66e138fb662893f68b6fed53700692450
Synchon Mandal <synchon@protonmail.com>
2025-08-20 17:34:03 +0200

    chore: add new global test fixture for maps testing

--- junifer/conftest.py
+++ junifer/conftest.py
@@ -1,10 +1,14 @@
 """Provide conftest for pytest."""
 
 # Authors: Federico Raimondo <f.raimondo@fz-juelich.de>
+#          Synchon Mandal <s.mandal@fz-juelich.de>
 # License: AGPL
+
+from pathlib import Path
 
 import pytest
 
+from junifer.datagrabber import PatternDataladDataGrabber
 from junifer.utils.singleton import Singleton
 
 
@@ -23,3 +27,31 @@ def reset_singletons() -> None:
     # Force deleting the singletons
     for elem in to_remove:
         del elem
+
+
+@pytest.fixture
+def maps_datagrabber(tmp_path: Path) -> PatternDataladDataGrabber:
+    """Return a PatternDataladDataGrabber for maps testing.
+
+    Parameters
+    ----------
+    tmp_path : pathlib.Path
+        The path to the test directory.
+
+    """
+    dg = PatternDataladDataGrabber(
+        uri="https://github.com/OpenNeuroDatasets/ds005226.git",
+        types=["BOLD"],
+        patterns={
+            "BOLD": {
+                "pattern": (
+                    "derivatives/pre-processed_data/space-MNI/{subject}/"
+                    "{subject-padded}_task-{task}_run-{run}_space-MNI152NLin6Asym"
+                    "_res-2_desc-preproc_bold.nii.gz"
+                ),
+                "space": "MNI152NLin6Asym",
+            },
+        },
+        replacements=["subject", "subject-padded", "task", "run"],
+    )
+    return dg
