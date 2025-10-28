1eaa1b803d18d7b8db4e0d71d8abffc38a6b616d
Synchon Mandal <synchon@protonmail.com>
2025-10-28 13:38:35 +0100

    update: introduce ExtDep enum

--- junifer/pipeline/__init__.pyi
+++ junifer/pipeline/__init__.pyi
@@ -3,6 +3,7 @@ __all__ = [
     "AssetLoaderDispatcher",
     "BaseDataDumpAsset",
     "DataObjectDumper",
+    "ExtDep",
     "PipelineComponentRegistry",
     "PipelineStepMixin",
     "UpdateMetaMixin",
@@ -19,5 +20,6 @@ from ._data_object_dumper import (
 from .pipeline_component_registry import PipelineComponentRegistry
 from .pipeline_step_mixin import PipelineStepMixin
 from .update_meta_mixin import UpdateMetaMixin
+from .utils import ExtDep
 from .workdir_manager import WorkDirManager
 from .marker_collection import MarkerCollection
