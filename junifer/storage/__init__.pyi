1a214e30c9b57a21d5c0b50fcaefe07535767e5b
Synchon Mandal <synchon@protonmail.com>
2025-10-28 18:00:07 +0100

    update: introduce MatrixKind enum

--- junifer/storage/__init__.pyi
+++ junifer/storage/__init__.pyi
@@ -4,9 +4,10 @@ __all__ = [
     "SQLiteFeatureStorage",
     "HDF5FeatureStorage",
     "StorageType",
+    "MatrixKind",
 ]
 
 from .pandas_base import PandasBaseFeatureStorage
 from .sqlite import SQLiteFeatureStorage
-from .base import BaseFeatureStorage, StorageType
+from .base import BaseFeatureStorage, MatrixKind, StorageType
 from .hdf5 import HDF5FeatureStorage
