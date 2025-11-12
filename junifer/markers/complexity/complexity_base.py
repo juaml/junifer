6044b870c7fd59686dd7bec0439b3d947d06b7f2
Synchon Mandal <synchon@protonmail.com>
2025-11-12 19:24:21 +0100

    refactor: make marker interface and impls pydantic models

--- junifer/markers/complexity/complexity_base.py
+++ junifer/markers/complexity/complexity_base.py
@@ -9,6 +9,7 @@ from typing import (
     TYPE_CHECKING,
     Any,
     ClassVar,
+    Literal,
 )
 
 from ...datagrabber import DataType
@@ -27,26 +28,27 @@ __all__ = ["ComplexityBase"]
 
 
 class ComplexityBase(BaseMarker):
-    """Base class for complexity computation.
+    """Abstract base class for complexity computation.
 
     Parameters
     ----------
-    parcellation : str or list of str
-        The name(s) of the parcellation(s). Check valid options by calling
-        :func:`junifer.data.parcellations.list_parcellations`.
+    parcellation : list of str
+        The name(s) of the parcellation(s) to use.
+        See :func:`.list_data` for options.
     agg_method : str, optional
-        The method to perform aggregation using. Check valid options in
-        :func:`junifer.stats.get_aggfunc_by_name` (default "mean").
-    agg_method_params : dict, optional
-        Parameters to pass to the aggregation function. Check valid options in
-        :func:`junifer.stats.get_aggfunc_by_name` (default None).
-    masks : str, dict or list of dict or str, optional
+        The aggregation function to use.
+        See :func:`.get_aggfunc_by_name` for options
+        (default "mean").
+    agg_method_params : dict or None, optional
+        The parameters to pass to the aggregation function.
+        See :func:`.get_aggfunc_by_name` for options (default None).
+    masks : list of dict or str, or None, optional
         The specification of the masks to apply to regions before extracting
         signals. Check :ref:`Using Masks <using_masks>` for more details.
         If None, will not apply any mask (default None).
-    name : str, optional
-        The name of the marker. If None, it will use the class name
-        (default None).
+    name : str or None, optional
+        The name of the marker.
+        If None, will use the class name (default None).
 
     """
 
@@ -58,19 +60,11 @@ class ComplexityBase(BaseMarker):
         },
     }
 
-    def __init__(
-        self,
-        parcellation: str | list[str],
-        agg_method: str = "mean",
-        agg_method_params: dict | None = None,
-        masks: str | dict | list[dict | str] | None = None,
-        name: str | None = None,
-    ) -> None:
-        self.parcellation = parcellation
-        self.agg_method = agg_method
-        self.agg_method_params = agg_method_params
-        self.masks = masks
-        super().__init__(on="BOLD", name=name)
+    parcellation: list[str]
+    agg_method: str = "mean"
+    agg_method_params: dict | None = None
+    masks: list[dict | str] | None = None
+    on: list[Literal[DataType.BOLD]] = [DataType.BOLD]  # noqa: RUF012
 
     @abstractmethod
     def compute_complexity(
@@ -118,7 +112,7 @@ class ComplexityBase(BaseMarker):
             method=self.agg_method,
             method_params=self.agg_method_params,
             masks=self.masks,
-            on="BOLD",
+            on=[DataType.BOLD],
         ).compute(input=input, extra_input=extra_input)
         # Compute complexity measure
         return {
