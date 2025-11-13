96b51ced364ba1a8972f5a7dead9c48e17bacdee
Synchon Mandal <synchon@protonmail.com>
2025-11-13 17:19:15 +0100

    update: fix tests for preprocess

--- junifer/preprocess/tests/test_temporal_filter.py
+++ junifer/preprocess/tests/test_temporal_filter.py
@@ -29,7 +29,7 @@ from junifer.testing.datagrabbers import PartlyCloudyTestingDataGrabber
             0.1,
             None,
             None,
-            "compute_brain_mask",
+            ["compute_brain_mask"],
         ],
         [
             True,
@@ -37,7 +37,7 @@ from junifer.testing.datagrabbers import PartlyCloudyTestingDataGrabber
             None,
             0.08,
             None,
-            "compute_background_mask",
+            ["compute_background_mask"],
         ],
         [
             False,
@@ -53,7 +53,7 @@ from junifer.testing.datagrabbers import PartlyCloudyTestingDataGrabber
             0.1,
             0.08,
             2,
-            "compute_brain_mask",
+            ["compute_brain_mask"],
         ],
     ),
 )
@@ -63,7 +63,7 @@ def test_TemporalFilter(
     low_pass: Optional[float],
     high_pass: Optional[float],
     t_r: Optional[float],
-    masks: Optional[str],
+    masks: Optional[list[str]],
 ) -> None:
     """Test TemporalFilter.
 
@@ -79,7 +79,7 @@ def test_TemporalFilter(
         The parametrized high pass value.
     t_r : float or None
         The parametrized repetition time.
-    masks : str or None
+    masks : list of str or None
         The parametrized mask.
 
     """
