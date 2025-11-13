5d4793b248d519b3b11cf9c46b986aa384e8bf6d
Synchon Mandal <synchon@protonmail.com>
2025-11-13 13:08:05 +0100

    update: introduce AOMICSpace and AOMICTask

--- junifer/datagrabber/aomic/__init__.pyi
+++ junifer/datagrabber/aomic/__init__.pyi
@@ -1,5 +1,12 @@
-__all__ = ["DataladAOMICID1000", "DataladAOMICPIOP1", "DataladAOMICPIOP2"]
+__all__ = [
+    "AOMICSpace",
+    "AOMICTask",
+    "DataladAOMICID1000",
+    "DataladAOMICPIOP1",
+    "DataladAOMICPIOP2",
+]
 
+from ._types import AOMICSpace, AOMICTask
 from .id1000 import DataladAOMICID1000
 from .piop1 import DataladAOMICPIOP1
 from .piop2 import DataladAOMICPIOP2
