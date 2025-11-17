d66c757d19a310b0019cb4bf1fb7e2f890184321
Synchon Mandal <synchon@protonmail.com>
2025-11-17 11:31:46 +0100

    chore: update types for juseless datagrabbers

--- junifer/configs/juseless/datagrabbers/__init__.pyi
+++ junifer/configs/juseless/datagrabbers/__init__.pyi
@@ -2,12 +2,13 @@ __all__ = [
     "JuselessDataladAOMICID1000VBM",
     "JuselessDataladCamCANVBM",
     "JuselessDataladIXIVBM",
+    "IXISite",
     "JuselessUCLA",
     "JuselessDataladUKBVBM",
 ]
 
 from .aomic_id1000_vbm import JuselessDataladAOMICID1000VBM
 from .camcan_vbm import JuselessDataladCamCANVBM
-from .ixi_vbm import JuselessDataladIXIVBM
+from .ixi_vbm import JuselessDataladIXIVBM, IXISite
 from .ucla import JuselessUCLA
 from .ukb_vbm import JuselessDataladUKBVBM
