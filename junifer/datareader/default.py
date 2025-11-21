0b47c3695a488e4ac1146f1bea1263ee26f8318e
Synchon Mandal <synchon@protonmail.com>
2025-11-21 11:26:20 +0100

    update: improve logging for datareader params

--- junifer/datareader/default.py
+++ junifer/datareader/default.py
@@ -9,15 +9,18 @@ from typing import Optional, Union
 
 import nibabel as nib
 import pandas as pd
+import structlog
 from pydantic import BaseModel
 
 from ..api.decorators import register_datareader
 from ..pipeline import PipelineStepMixin, UpdateMetaMixin
-from ..utils.logging import logger, warn_with_log
+from ..utils.logging import warn_with_log
 
 
 __all__ = ["DefaultDataReader"]
 
+_log = structlog.get_logger("junifer")
+logger = _log.bind(pkg="datareader", step="datareader")
 
 # Map each file extension to a type
 _extensions = {
