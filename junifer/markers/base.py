1fdc6f6631ab42a37bb92550f7fff1666b5399ba
Synchon Mandal <synchon@protonmail.com>
2026-07-17 13:23:16 +0200

    fix: correct "on" argument type for BaseMarker

--- junifer/markers/base.py
+++ junifer/markers/base.py
@@ -6,16 +6,16 @@
 
 from abc import ABC, abstractmethod
 from copy import deepcopy
-from typing import Any, ClassVar
+from typing import Annotated, Any, ClassVar
 
 import structlog
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, BeforeValidator, ConfigDict
 
 from ..datagrabber import DataType
 from ..pipeline import PipelineStepMixin, UpdateMetaMixin
 from ..storage import StorageType
 from ..typing import MarkerInOutMappings, StorageLike
-from ..utils import raise_error
+from ..utils import ensure_list_or_none, raise_error
 
 
 __all__ = ["BaseMarker"]
@@ -57,7 +57,10 @@ class BaseMarker(BaseModel, ABC, PipelineStepMixin, UpdateMetaMixin):
 
     model_config = ConfigDict(use_enum_values=True)
 
-    on: list[DataType] | None = None
+    on: Annotated[
+        DataType | list[DataType] | None,
+        BeforeValidator(ensure_list_or_none),
+    ] = None
     name: str | None = None
 
     def model_post_init(self, context: Any):  # noqa: D102
